86725145df4e3b29463e21f70870d1bc5010c1d2
Osma Suominen <osma.suominen@aalto.fi>
2014-10-22 16:09:42 +0300

    merge JUHO hierarchy with YSO hierarchy

--- vocabularies/juho/toskos.sh
+++ vocabularies/juho/toskos.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
-INFILES="juho-metadata.ttl juhoKehitys.ttl"
+INFILE="juhoKehitys.ttl"
+INFILES="juho-metadata.ttl -"
 OUTFILE=juho-skos.ttl
 
-SKOSIFYHOME="../../tools/skosify/"
+MERGEHIER="../../tools/merge-hierarchy/merge-hierarchy.py"
+SKOSIFYHOME="../../tools/skosify"
 CONFFILE=$SKOSIFYHOME/finnonto.cfg
 LOGFILE=skosify.log
-OPTS="-l fi"
+OPTS="-l fi -f turtle"
 
-$SKOSIFYHOME/skosify.py $OPTS -c $CONFFILE $INFILES -o $OUTFILE 2>$LOGFILE
+$MERGEHIER $INFILE | $SKOSIFYHOME/skosify.py $OPTS -c $CONFFILE -o $OUTFILE $INFILES 2>$LOGFILE
