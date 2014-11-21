9b0b0200050260596ec2345e528ce5f97e2e7fe8
Osma Suominen <osma.suominen@helsinki.fi>
2014-11-21 12:04:02 +0200

    script for merging MUSA and CILLA data

---
+++ tools/oai-pmh-to-skos/merge-musa-cilla.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from rdflib import Graph, Namespace, URIRef, Literal, RDF, RDFS
+import sys
+
+musa = Graph()
+musa.parse(sys.argv[1], format='turtle')
+
+cilla = Graph()
+cilla.parse(sys.argv[2], format='turtle')
+
+SKOS=Namespace("http://www.w3.org/2004/02/skos/core#")
+CILLA=Namespace("http://www.yso.fi/onto/cilla/")
+
+# figure out the equivalences
+cilla_to_musa = {}
+
+def find_label(voc, conc, lang):
+    labels = voc.preferredLabel(conc, lang=lang)
+    if len(labels) != 0:
+        return labels[0][1]
+    return None
+
+def find_link(src, dst, conc, lang):
+    label = find_label(src, conc, lang)
+    dconc = dst.value(None, SKOS.prefLabel, label)
+    if dconc is None:
+        dconc = dst.value(None, SKOS.altLabel, label)
+    return dconc
+
+for mconc in musa.subjects(RDF.type, SKOS.Concept):
+    for linklang in ['sv','fi']:
+        cconc = find_link(musa, cilla, mconc, linklang)
+        if cconc is not None:
+            cilla_to_musa[cconc] = mconc
+            break
+
+for cconc in cilla.subjects(RDF.type, SKOS.Concept):
+    if cconc in cilla_to_musa: continue # already linked
+    for linklang in ['sv','fi']:
+        mconc = find_link(cilla, musa, cconc, linklang)
+        if mconc is not None:
+            cilla_to_musa[cconc] = mconc
+            break
+
+# remove Swedish data from MUSA
+for s,p,o in musa.triples((None,None,None)):
+    if isinstance(o, Literal) and o.language == 'sv':
+        musa.remove((s,p,o))
+
+# merge Cilla data to MUSA
+for cconc in cilla.subjects(RDF.type, SKOS.Concept):
+    mconc = cilla_to_musa.get(cconc, None)
+    if mconc is None: continue
+    for p,o in cilla.predicate_objects(cconc):
+        if o.startswith(CILLA):
+            if o in cilla_to_musa:
+                o = cilla_to_musa[o]
+            else:
+                continue
+        if isinstance(o, Literal) and o.language == 'fi':
+            continue
+        musa.add((mconc, p, o))
+
+musa.serialize(destination=sys.stdout, format='turtle')
