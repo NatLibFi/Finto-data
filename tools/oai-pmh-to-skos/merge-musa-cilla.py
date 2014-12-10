c81cbd7bcd7e8347b60cf551858545c3fc84d8aa
Osma Suominen <osma.suominen@helsinki.fi>
2014-12-10 12:12:26 +0200

    avoid multiple timestamps; complain about CILLA concepts with no equivalent MUSA concept

--- tools/oai-pmh-to-skos/merge-musa-cilla.py
+++ tools/oai-pmh-to-skos/merge-musa-cilla.py
@@ -11,6 +11,7 @@ cilla = Graph()
 cilla.parse(sys.argv[2], format='turtle')
 
 SKOS=Namespace("http://www.w3.org/2004/02/skos/core#")
+DCT=Namespace("http://purl.org/dc/terms/")
 CILLA=Namespace("http://www.yso.fi/onto/cilla/")
 
 # figure out the equivalences
@@ -52,7 +53,10 @@ for s,p,o in musa.triples((None,None,None)):
 # merge Cilla data to MUSA
 for cconc in cilla.subjects(RDF.type, SKOS.Concept):
     mconc = cilla_to_musa.get(cconc, None)
-    if mconc is None: continue
+    if mconc is None:
+        print >>sys.stderr, ("No MUSA concept found for CILLA concept %s '%s'" % (cconc, find_label(cilla, cconc, 'sv'))).encode('UTF-8')
+        continue
+
     for p,o in cilla.predicate_objects(cconc):
         if o.startswith(CILLA):
             if o in cilla_to_musa:
@@ -61,6 +65,23 @@ for cconc in cilla.subjects(RDF.type, SKOS.Concept):
                 continue
         if isinstance(o, Literal) and o.language == 'fi':
             continue
+
+        if p == DCT.created:
+            musats = musa.value(mconc, p, None)
+            if musats > o:
+                musa.remove((mconc, p, musats))
+                # use the older timestamp from CILLA instead
+            else:
+                continue # use the timestamp from MUSA, forget this CILLA ts
+
+        if p == DCT.modified:
+            musats = musa.value(mconc, p, None)
+            if musats < o:
+                musa.remove((mconc, p, musats))
+                # use the newer timestamp from CILLA instead
+            else:
+                continue # use the timestamp from MUSA, forget this CILLA ts
+
         musa.add((mconc, p, o))
 
 musa.serialize(destination=sys.stdout, format='turtle')
