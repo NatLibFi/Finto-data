e14e2694e33af0c4b12462cdd7eeb57db31d00dd
Osma Suominen <osma.suominen@helsinki.fi>
2015-03-13 09:58:34 +0200

    AFO julkaisuversio filtteröitynä ja YSOn kera

--- vocabularies/afo/toskos.sh
+++ vocabularies/afo/toskos.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
-INFILES="afo-metadata.ttl afoKehitys.ttl"
+JENAHOME="$HOME/sw/apache-jena"
+INFILES="afo-metadata.ttl afoKehitys.ttl afo-excluded.nt yso-afo.ttl"
 OUTFILE=afo-skos.ttl
+
+$JENAHOME/bin/arq --data=afoKehitys.ttl --query=filter-afo.sparql --results=NT >afo-excluded.nt
 
 SKOSIFYHOME="../../tools/skosify/"
 CONFFILE=$SKOSIFYHOME/finnonto.cfg
