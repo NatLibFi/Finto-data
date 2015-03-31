42c458ea7f192d43050b424eaaed216190300563
Osma Suominen <osma.suominen@helsinki.fi>
2015-03-31 15:41:44 +0300

    JUHOn SKOS-muunnos uusittu; uuden version julkaisu

--- vocabularies/juho/toskos.sh
+++ vocabularies/juho/toskos.sh
@@ -1,13 +1,19 @@
 #!/bin/sh
 
-INFILE="juhoKehitys.ttl"
-INFILES="juho-metadata.ttl -"
+JENAHOME="$HOME/sw/apache-jena"
+INFILE="juho.ttl"
+YSOFILE="yso-juho.ttl"
+HIERFILE="juho-hierarchy.nt"
+MERGEHIER="../../tools/merge-hierarchy/merge-hierarchy.sparql"
+
+$JENAHOME/bin/arq --data=$INFILE --data=$YSOFILE --query=$MERGEHIER --results=NT >$HIERFILE
+
+INFILES="juho-metadata.ttl $HIERFILE $INFILE $YSOFILE"
 OUTFILE=juho-skos.ttl
 
-MERGEHIER="../../tools/merge-hierarchy/merge-hierarchy.py"
 SKOSIFYHOME="../../tools/skosify"
 CONFFILE=$SKOSIFYHOME/finnonto.cfg
 LOGFILE=skosify.log
 OPTS="-l fi -f turtle"
 
-$MERGEHIER $INFILE | $SKOSIFYHOME/skosify.py $OPTS -c $CONFFILE -o $OUTFILE $INFILES 2>$LOGFILE
+$SKOSIFYHOME/skosify.py $OPTS -c $CONFFILE -o $OUTFILE $INFILES 2>$LOGFILE
