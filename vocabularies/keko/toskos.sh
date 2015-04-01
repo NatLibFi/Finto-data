b2c3d67d6720a46ea6d459c18a9bf1b344ee3c96
Osma Suominen <osma.suominen@helsinki.fi>
2015-04-01 12:44:38 +0300

    KEKOn hierarkian täydennys SPARQL-kyselyllä

---
+++ vocabularies/keko/toskos.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+JENAHOME="$HOME/sw/apache-jena"
+INFILE="keko-skos.orig.ttl"
+HIERFILE="keko-hierarchy.nt"
+MERGEHIER="../../tools/merge-hierarchy/merge-hierarchy-skos.sparql"
+
+$JENAHOME/bin/arq --data=$INFILE --query=$MERGEHIER --results=NT >$HIERFILE
+
+INFILES="$HIERFILE $INFILE"
+OUTFILE=keko-skos.ttl
+
+$JENAHOME/bin/rdfcat -out ttl $INFILES >$OUTFILE
