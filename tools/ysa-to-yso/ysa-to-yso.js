b9a8eb2de2160fa46dc52da4b5da24840d2d32f7
Osma Suominen <osma.suominen@helsinki.fi>
2015-06-08 16:15:26 +0300

    fix sparql VALUES syntax

--- tools/ysa-to-yso/ysa-to-yso.js
+++ tools/ysa-to-yso/ysa-to-yso.js
@@ -102,7 +102,7 @@ function refreshUpdateQuery() {
                 var vals = $.map(vars, function(varname) {
                         return node_to_value(row[varname]);
                 });
-                return vals.join(" ");
+                return "( " + vals.join(" ") + " )";
         });
         var newquery = query.replace(valuesRegExp, matches[1] + "\n" + values.join("\n") + "\n" + matches[4]);
         console.log(newquery);
