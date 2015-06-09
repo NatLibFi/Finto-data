ce7e3146c92a15096fa94e78fcbb03562b17a7c4
Osma Suominen <osma.suominen@helsinki.fi>
2015-06-09 13:00:56 +0300

    AFOn julkaisuversio

--- vocabularies/afo/toskos.sh
+++ vocabularies/afo/toskos.sh
@@ -1,13 +1,10 @@
 #!/bin/sh
 
-JENAHOME="$HOME/sw/apache-jena"
-INFILES="afo-metadata.ttl afoKehitys.ttl afo-excluded.nt yso-afo.ttl"
+INFILES="afo-metadata.ttl afoKehitys.ttl yso-afo.ttl"
 OUTFILE=afo-skos.ttl
 
-$JENAHOME/bin/arq --data=afoKehitys.ttl --query=filter-afo.sparql --results=NT >afo-excluded.nt
-
-SKOSIFYHOME="../../tools/skosify/"
+SKOSIFYHOME="../../../Skosify/"
 CONFFILE=$SKOSIFYHOME/finnonto.cfg
 LOGFILE=skosify.log
 
-$SKOSIFYHOME/skosify.py -c $CONFFILE $INFILES -o $OUTFILE 2>$LOGFILE
+$SKOSIFYHOME/skosify/skosify.py -c $CONFFILE $INFILES -o $OUTFILE 2>$LOGFILE
