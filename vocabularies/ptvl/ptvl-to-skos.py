02b8527a92acab3e9a39e91741ecb122ad4d0d03
Osma Suominen <osma.suominen@helsinki.fi>
2015-06-17 12:12:08 +0300

    Palvelutietovarantojen luokitus SKOSina

---
+++ vocabularies/ptvl/ptvl-to-skos.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+from rdflib import Graph, Namespace, URIRef, Literal, RDF
+import csv
+import sys
+import re
+
+if len(sys.argv) != 2:
+    print >>sys.stderr, "Usage: %s <csvfile>" % sys.argv[0]
+    sys.exit(1)
+
+csvfile = sys.argv[1]
+
+PTVL = Namespace("http://urn.fi/URN:NBN:fi:au:ptvl:")
+SKOS = Namespace("http://www.w3.org/2004/02/skos/core#")
+
+g = Graph()
+g.namespace_manager.bind('skos', SKOS)
+g.namespace_manager.bind('ptvl', PTVL)
+
+def add_concept(cell):
+    notation, label = cell.split(". ", 1)
+    uri = PTVL[notation]
+    g.add((uri, RDF.type, SKOS.Concept))
+    g.add((uri, SKOS.notation, Literal(notation)))
+    g.add((uri, SKOS.prefLabel, Literal(label, 'fi')))
+    m = re.search('^\D+', notation)
+    schemeuri = PTVL[m.group(0)]
+    g.add((uri, SKOS.inScheme, schemeuri))
+
+    if '.' in notation:
+        parent = notation.rsplit('.', 1)[0]
+        parenturi = PTVL[parent]
+        g.add((uri, SKOS.broader, parenturi))
+        g.add((parenturi, SKOS.narrower, uri))
+    else:
+        g.add((uri, SKOS.topConceptOf, schemeuri))
+        g.add((schemeuri, SKOS.hasTopConcept, uri))
+
+
+with open(csvfile, 'rb') as cf:
+    reader = csv.reader(cf)
+    for row in reader:
+        for cell in row:
+            if cell.strip() != '':
+                add_concept(cell.strip())
+
+g.serialize(destination=sys.stdout, format='turtle')
---
+++ vocabularies/ptvl/toskos.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+DATAFILES="palveluluokat.ttl kohderyhmat.ttl elamantilanteet.ttl tuottajatyypit.ttl toteutustavat.ttl"
+INFILES="ptvl-metadata.ttl $DATAFILES"
+OUTFILE=ptvl-skos.ttl
+
+for datafile in $DATAFILES; do
+	csvfile=`basename $datafile .ttl`.csv
+	echo $csvfile
+	./ptvl-to-skos.py $csvfile >$datafile
+done
+
+SKOSIFYHOME="../../../Skosify/"
+LOGFILE=skosify.log
+OPTS=""
+
+$SKOSIFYHOME/skosify/skosify.py $OPTS $INFILES -o $OUTFILE 2>$LOGFILE
