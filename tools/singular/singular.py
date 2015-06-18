840470b6aa8a0437e5852287511a33f07e01ba2a
Osma Suominen <osma.suominen@helsinki.fi>
2015-06-18 15:05:08 +0300

    only create baseforms of concepts with terms that look like plurals

--- tools/singular/singular.py
+++ tools/singular/singular.py
@@ -10,7 +10,7 @@ SKOS = Namespace('http://www.w3.org/2004/02/skos/core#')
 YSO = Namespace("http://www.yso.fi/onto/yso/")
 
 APIBASE="http://demo.seco.tkk.fi/las/"
-LANGUAGES=('fi', 'sv', 'en')
+PLURAL_SUFFIXES = {'fi': 't', 'sv': 'r', 'en': 's'}
 
 def singular(label, lang):
     url = APIBASE + "baseform"
@@ -25,20 +25,26 @@ def singular(label, lang):
 g = Graph()
 g.parse(sys.argv[1], format='turtle')
 
-concepts = set(g.subjects(SKOS.inScheme, YSO['']))
-sample = random.sample(concepts, 1000)
+concepts = list(g.subjects(SKOS.inScheme, YSO['']))
+random.shuffle(concepts)
 
 writer = csv.writer(sys.stdout)
+i = 0
 
-
-for conc in sample:
+for conc in concepts:
     vals = [conc]
-    for lang in LANGUAGES:
+    is_plural = False
+    for lang,pl_suffix in PLURAL_SUFFIXES.items():
         try:
             label = g.preferredLabel(conc, lang=lang)[0][1]
+            if label.endswith(pl_suffix):
+                is_plural = True
             labelSingular = singular(label, lang)
         except:
             label = labelSingular = ''
         vals += [label.encode('UTF-8'), labelSingular.encode('UTF-8')]
+    if not is_plural:
+        continue
     writer.writerow(vals)
-        
+    i += 1
+    if i >= 1000: break
