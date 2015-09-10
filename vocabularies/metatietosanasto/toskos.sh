dcb690d741878e1d1cc8ed1433d2b10a17d31f26
Osma Suominen <osma.suominen@helsinki.fi>
2015-09-10 16:40:20 +0300

    metatietosanasto päivitetty; ryhmähierarkia vaihdettu skos:memberiksi julkaisuversiossa

--- vocabularies/metatietosanasto/toskos.sh
+++ vocabularies/metatietosanasto/toskos.sh
@@ -5,6 +5,6 @@ OUTFILE=metatietosanasto-skos.ttl
 
 SKOSIFYHOME="../../../Skosify/skosify/"
 LOGFILE=skosify.log
-OPTS="-c metatietosanasto.cfg -f turtle"
+OPTS="-c metatietosanasto.cfg -f turtle --update-query @supergroup-to-member.rq"
 
 $SKOSIFYHOME/skosify.py $OPTS -o $OUTFILE $INFILES 2>$LOGFILE
