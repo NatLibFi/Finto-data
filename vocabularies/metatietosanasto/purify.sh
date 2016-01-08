f9769d9899957e7e4ceb93e417006be636d50c4d
Osma Suominen <osma.suominen@helsinki.fi>
2016-01-08 14:42:43 +0200

    puritettu metatietosanasto

---
+++ vocabularies/metatietosanasto/purify.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+ONTOFILE=metatietosanasto.ttl
+FORMAT=turtle
+
+PURIFY=../../tools/purify/purify.py
+ONTONS=http://urn.fi/URN:NBN:fi:au:mts:
+PURICHAR=m
+ONTOPURIBASE="${ONTONS}${PURICHAR}"
+CONTEXT=$ONTOPURIBASE
+OUTFILE="${ONTOFILE}.new"
+
+$PURIFY -c $CONTEXT -s $PURICHAR -f $FORMAT -t $FORMAT $ONTOFILE $ONTONS $ONTOPURIBASE >$OUTFILE
+if [ -s $OUTFILE ]; then
+  mv $OUTFILE $ONTOFILE
+  echo "Done, replaced $ONTOFILE with purified version."
+  echo "Remember to load and re-save using TBC to preserve file order!"
+fi
