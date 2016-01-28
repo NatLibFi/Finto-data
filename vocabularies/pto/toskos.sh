7138b8af21b99a94ee5f2e687e5aac6eb7c4736c
Osma Suominen <osma.suominen@helsinki.fi>
2016-01-28 15:17:53 +0200

    PTO:n uusi versio Saku Seppälältä 28.1.2016

--- vocabularies/pto/toskos.sh
+++ vocabularies/pto/toskos.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 JENAHOME="$HOME/ontology/sw/apache-jena"
-INFILE="paikkatieto-ei45-eiinspire-puritettu-ylakas-pois.ttl"
+INFILE="paikkatieto_FintoJulkaisu_2015_12.ttl"
 MERGEHIER="../../Finto-tools/merge-hierarchy/merge-hierarchy.sparql"
 
 $JENAHOME/bin/arq --data=$INFILE --data=yso-paikkatieto.ttl --query=$MERGEHIER --results=NT >pto-hierarchy.nt
@@ -11,6 +11,6 @@ OUTFILE=pto-skos.ttl
 
 SKOSIFYHOME="../../SKOSIFY/"
 LOGFILE=skosify.log
-OPTS="-c pto2skos.cfg"
+OPTS="-D -c pto2skos.cfg"
 
 $SKOSIFYHOME/skosify.py $OPTS -o $OUTFILE $INFILES 2>$LOGFILE
