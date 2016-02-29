a47369b6559ec7f8489df63dbabb80648e17d5d0
Osma Suominen <osma.suominen@helsinki.fi>
2016-02-29 14:15:37 +0200

    remove special step for correcting hierarchy, as it is now done within Skosify

--- vocabularies/juho/toskos.sh
+++ vocabularies/juho/toskos.sh
@@ -1,19 +1,14 @@
 #!/bin/sh
 
-JENAHOME="$HOME/sw/apache-jena"
 INFILE="juho.ttl"
 YSOFILE="../yso/ysoKehitys.rdf"
-HIERFILE="juho-hierarchy.nt"
-MERGEHIER="../../tools/merge-hierarchy/merge-hierarchy.sparql"
 
-$JENAHOME/bin/arq --data=$INFILE --data=$YSOFILE --query=$MERGEHIER --results=NT >$HIERFILE
-
-INFILES="juho-metadata.ttl $HIERFILE $INFILE $YSOFILE"
+INFILES="juho-metadata.ttl $INFILE $YSOFILE"
 OUTFILE=juho-skos.ttl
 
-SKOSIFYHOME="../../tools/skosify"
-CONFFILE=$SKOSIFYHOME/finnonto.cfg
+SKOSIFYHOME="../../../Skosify"
+CONFFILE=../../conf/skosify/finnonto.cfg
 LOGFILE=skosify.log
 OPTS="-l fi"
 
-$SKOSIFYHOME/skosify.py $OPTS -c $CONFFILE -o $OUTFILE $INFILES 2>$LOGFILE
+$SKOSIFYHOME/skosify/skosify.py $OPTS -c $CONFFILE -o $OUTFILE $INFILES 2>$LOGFILE
