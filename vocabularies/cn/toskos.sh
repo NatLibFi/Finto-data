95216c9eb27a7df524dcec9fd4d1ab44b4a54251
Osma Suominen <osma.suominen@helsinki.fi>
2016-03-03 13:41:46 +0200

    fix path

--- vocabularies/cn/toskos.sh
+++ vocabularies/cn/toskos.sh
@@ -3,8 +3,8 @@
 INFILES="cn-metadata.ttl rdaa.ttl rdac.ttl cn.ttl"
 OUTFILE=cn-skos.ttl
 
-SKOSIFYHOME="../../tools/skosify/"
+SKOSIFYHOME="../../tools/skosify"
 LOGFILE=skosify.log
 OPTS="--no-mark-top-concepts --namespace http://urn.fi/URN:NBN:fi:au:cn:"
 
-$SKOSIFYHOME/skosify/skosify.py $OPTS $INFILES -o $OUTFILE 2>$LOGFILE
+$SKOSIFYHOME/skosify.py $OPTS $INFILES -o $OUTFILE 2>$LOGFILE
