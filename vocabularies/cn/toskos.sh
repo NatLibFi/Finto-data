3f9be9c80873a33305e142c7d251f0b94728da11
Osma Suominen <osma.suominen@helsinki.fi>
2016-03-03 12:50:00 +0200

    lisätty LAS-välimuisti, päivitetty toimimaan uusimmalle datalle, parempaa debuggausta

--- vocabularies/cn/toskos.sh
+++ vocabularies/cn/toskos.sh
@@ -3,10 +3,10 @@
 INFILES="cn-metadata.ttl rdaa.ttl rdac.ttl cn.ttl"
 OUTFILE=cn-skos.ttl
 
-SKOSIFYHOME="../../tools/skosify/"
+SKOSIFYHOME="../../../Skosify/"
 LOGFILE=skosify.log
 CNLOGFILE=cn.log
 OPTS="--no-mark-top-concepts --namespace http://urn.fi/URN:NBN:fi:au:cn:"
 
 ./convert-cn-to-skos.py >cn.ttl 2>$CNLOGFILE
-$SKOSIFYHOME/skosify.py $OPTS $INFILES -o $OUTFILE 2>$LOGFILE
+$SKOSIFYHOME/skosify/skosify.py $OPTS $INFILES -o $OUTFILE 2>$LOGFILE
