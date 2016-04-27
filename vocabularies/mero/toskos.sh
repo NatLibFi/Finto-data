b57235a6b5268f29354f32e373e499bc32eb1d38
henriyli <henri.ylikotila@helsinki.fi>
2016-04-27 10:50:46 +0300

    skosifaijattu mero

--- vocabularies/mero/toskos.sh
+++ vocabularies/mero/toskos.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-INFILES="mero-metadata.ttl merisanastox110309.rdf-xml-ONKIin.owl"
+INFILES="mero-metadata.ttl mero.ttl"
 OUTFILE=mero-skos.ttl
 
 SKOSIFYHOME="../../tools/skosify/"
-CONFFILE=$SKOSIFYHOME/finnonto.cfg
+CONFFILE=../../conf/skosify/finnonto.cfg
 LOGFILE=skosify.log
 
 $SKOSIFYHOME/skosify.py -c $CONFFILE $INFILES -o $OUTFILE 2>$LOGFILE
