5cfd4796bd7047f763138f77398ccf2d35f59901
henriyli <henri.ylikotila@helsinki.fi>
2016-06-20 10:48:49 +0300

    lisätään päivitetty ja puritettu puho

--- vocabularies/puho/toskos.sh
+++ vocabularies/puho/toskos.sh
@@ -4,7 +4,7 @@ INFILES="puho-metadata.ttl puho.ttl yso-puho.ttl"
 OUTFILE=puho-skos.ttl
 
 SKOSIFYHOME="../../../Skosify/"
-CONFFILE=$SKOSIFYHOME/finnonto.cfg
+CONFFILE=../../conf/skosify/finnonto.cfg
 LOGFILE=skosify.log
 
 $SKOSIFYHOME/skosify/skosify.py -c $CONFFILE $INFILES -o $OUTFILE 2>$LOGFILE
