90b795f0d5b11304a49ef365d2a7a6760b16d595
henriyli <henri.ylikotila@helsinki.fi>
2016-09-02 10:40:12 +0300

    ajettu uusi importillinen kauno skosifyn läpi

--- vocabularies/kauno/toskos.sh
+++ vocabularies/kauno/toskos.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-
-INFILES="kauno-metadata.ttl kauno.ttl yso-kauno.ttl"
+YSOFILE="../yso/ysoKehitys.rdf"
+INFILES="kauno-metadata.ttl kauno.ttl $YSOFILE"
 OUTFILE=kauno-skos.ttl
 
 SKOSIFYHOME="../../tools/skosify/"
@@ -9,3 +9,4 @@ CONFFILE="../../conf/skosify/finnonto.cfg"
 LOGFILE=skosify.log
 
 $SKOSIFYHOME/skosify.py -c $CONFFILE $INFILES -o $OUTFILE 2>$LOGFILE
+
