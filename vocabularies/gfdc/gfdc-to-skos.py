060c5950473cefceea3ef754b8f3a715b40589c4
Osma Suominen <osma.suominen@aalto.fi>
2016-09-28 17:47:57 +0300

    ensimmäinen versio GFDC-luokituksesta SKOSina (pelkät luokat, preflabelit ja hierarkia)

---
+++ vocabularies/gfdc/gfdc-to-skos.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+
+from rdflib import Graph, Namespace, URIRef, Literal, RDF
+import csv
+import sys
+import re
+
+if len(sys.argv) != 2:
+    print >>sys.stderr, "Usage: %s <csvfile>" % sys.argv[0]
+    sys.exit(1)
+
+csvfile = sys.argv[1]
+
+GFDC = Namespace("http://urn.fi/URN:NBN:fi:au:gfdc:")
+SKOS = Namespace("http://www.w3.org/2004/02/skos/core#")
+
+g = Graph()
+g.namespace_manager.bind('skos', SKOS)
+g.namespace_manager.bind('gfdc', GFDC)
+
+def add_class(notation, labelEn, labelFi, labelDe, labelFr):
+    uri = GFDC[notation]
+    g.add((uri, RDF.type, SKOS.Concept))
+    g.add((uri, SKOS.notation, Literal(notation)))
+    if labelEn != '' and labelEn != 'MISSING_VALUE':
+        g.add((uri, SKOS.prefLabel, Literal(labelEn, 'en')))
+    if labelFi != '' and labelFi != 'MISSING_VALUE':
+        g.add((uri, SKOS.prefLabel, Literal(labelFi, 'fi')))
+    if labelDe != '' and labelDe != 'MISSING_VALUE':
+        g.add((uri, SKOS.prefLabel, Literal(labelDe, 'de')))
+    if labelFr != '' and labelFr != 'MISSING_VALUE':
+        g.add((uri, SKOS.prefLabel, Literal(labelFr, 'fr')))
+    
+    parent = notation[:-1]
+    if parent.endswith('.'):
+        parent = parent[:-1]
+    if parent != '':
+        parenturi = GFDC[parent]
+        g.add((uri, SKOS.broader, parenturi))
+        g.add((parenturi, SKOS.narrower, uri))
+    else:
+        g.add((uri, SKOS.topConceptOf, GFDC['']))
+        g.add((GFDC[''], SKOS.hasTopConcept, uri))
+
+
+with open(csvfile, 'rb') as cf:
+    reader = csv.DictReader(cf)
+    for row in reader:
+        add_class(row['fdcNumber'].strip(),
+                  row['prefLabel-eng'].strip(),
+                  row['prefLabel-fin'].strip(),
+                  row['prefLabel-ger'].strip(),
+                  row['prefLabel-fre'].strip())
+
+g.serialize(destination=sys.stdout, format='turtle')
---
+++ vocabularies/gfdc/toskos.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+./gfdc-from-docs.py
+
+INFILES="gfdc-metadata.ttl gfdc.ttl"
+OUTFILE=ptvl-skos.ttl
+
+./gfdc-to-skos.py classes.csv >gfdc.ttl
+
+SKOSIFYHOME="../../../Skosify/"
+LOGFILE=skosify.log
+OPTS="--set-modified"
+
+$SKOSIFYHOME/skosify/skosify.py $OPTS $INFILES -o $OUTFILE 2>$LOGFILE
