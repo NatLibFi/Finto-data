fa2bb1fd6279fb87ae998945f241564f1b06f48a
Osma Suominen <osma.suominen@helsinki.fi>
2016-10-05 18:01:38 +0300

    add includingNotes and scopeNotes to SKOS output

--- vocabularies/gfdc/gfdc-to-skos.py
+++ vocabularies/gfdc/gfdc-to-skos.py
@@ -14,6 +14,14 @@ csvfile = sys.argv[1]
 GFDC = Namespace("http://urn.fi/URN:NBN:fi:au:gfdc:")
 SKOS = Namespace("http://www.w3.org/2004/02/skos/core#")
 
+# map 3-letter ISO 639-2 language codes to 2-letter 639-1 codes used in RDF
+LANGMAP = {
+    'eng': 'en',
+    'fin': 'fi',
+    'ger': 'de',
+    'fre': 'fr'
+}
+
 g = Graph()
 g.namespace_manager.bind('skos', SKOS)
 g.namespace_manager.bind('gfdc', GFDC)
@@ -21,18 +29,17 @@ g.namespace_manager.bind('gfdc', GFDC)
 def class_uri(notation):
     return GFDC['C' + notation]
 
-def add_class(notation, labelEn, labelFi, labelDe, labelFr):
+def add_class(notation, labels, includingNotes, scopeNotes):
     uri = class_uri(notation)
     g.add((uri, RDF.type, SKOS.Concept))
     g.add((uri, SKOS.notation, Literal(notation)))
-    if labelEn != '' and labelEn != 'MISSING_VALUE':
-        g.add((uri, SKOS.prefLabel, Literal(labelEn, 'en')))
-    if labelFi != '' and labelFi != 'MISSING_VALUE':
-        g.add((uri, SKOS.prefLabel, Literal(labelFi, 'fi')))
-    if labelDe != '' and labelDe != 'MISSING_VALUE':
-        g.add((uri, SKOS.prefLabel, Literal(labelDe, 'de')))
-    if labelFr != '' and labelFr != 'MISSING_VALUE':
-        g.add((uri, SKOS.prefLabel, Literal(labelFr, 'fr')))
+    for lang3, lang2 in LANGMAP.items():
+        if labels[lang3] != '' and labels[lang3] != 'MISSING_VALUE':
+            g.add((uri, SKOS.prefLabel, Literal(labels[lang3], lang2)))
+        if includingNotes[lang3] != '':
+            g.add((uri, SKOS.scopeNote, Literal(includingNotes[lang3], lang2)))
+        if scopeNotes[lang3] != '':
+            g.add((uri, SKOS.scopeNote, Literal(scopeNotes[lang3], lang2)))
     
     parent = notation[:-1]
     if parent.endswith('.'):
@@ -48,10 +55,13 @@ def add_class(notation, labelEn, labelFi, labelDe, labelFr):
 with open(csvfile, 'rb') as cf:
     reader = csv.DictReader(cf)
     for row in reader:
-        add_class(row['fdcNumber'].strip(),
-                  row['prefLabel-eng'].strip(),
-                  row['prefLabel-fin'].strip(),
-                  row['prefLabel-ger'].strip(),
-                  row['prefLabel-fre'].strip())
+        labels = {}
+        includingNotes = {}
+        scopeNotes = {}
+        for lang in LANGMAP.keys():
+            labels[lang] = row['prefLabel-%s' % lang].strip()
+            includingNotes[lang] = row['includingNote-%s' % lang].strip()
+            scopeNotes[lang] = row['scopeNote-%s' % lang].strip()
+        add_class(row['fdcNumber'].strip(), labels, includingNotes, scopeNotes)
 
 g.serialize(destination=sys.stdout, format='turtle')
