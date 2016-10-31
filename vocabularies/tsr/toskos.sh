d24574600edbb3a1d2c95c8995b1ba57c8e3c9b0
henriyli <henri.ylikotila@helsinki.fi>
2016-10-31 15:47:58 +0200

    päivitetään tsr

--- vocabularies/tsr/toskos.sh
+++ vocabularies/tsr/toskos.sh
@@ -4,7 +4,7 @@ INFILES="tsr-metadata.ttl tsr.owl"
 OUTFILE=tsr-skos.ttl
 
 SKOSIFYHOME="../../tools/skosify/"
-CONFFILE=$SKOSIFYHOME/finnonto.cfg
+CONFFILE=../../conf/skosify/finnonto.cfg
 LOGFILE=skosify.log
 
 $SKOSIFYHOME/skosify.py -c $CONFFILE $INFILES -o $OUTFILE 2>$LOGFILE
