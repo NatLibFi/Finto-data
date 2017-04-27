93aacf529e84e0124c490f714308403c0d9ac2b9
Henri Ylikotila <henri.ylikotila@helsinki.fi>
2017-04-27 09:52:46 +0300

    korjataan genresanastoa

--- vocabularies/genre/toskos.sh
+++ vocabularies/genre/toskos.sh
@@ -5,6 +5,6 @@ OUTFILE=genre-skos.ttl
 
 SKOSIFYHOME="../../../Skosify"
 LOGFILE=skosify.log
-OPTS="--namespace http://urn.fi/URN:NBN:fi:au:mts: -c ../metatietosanasto/metatietosanasto.cfg"
+OPTS="--namespace http://urn.fi/URN:NBN:fi:au:mts: -c ./genresanasto.cfg"
 
 $SKOSIFYHOME/skosify/skosify.py $OPTS $INFILES -o $OUTFILE 2>$LOGFILE
