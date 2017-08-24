b89d1121625794767fb8bca81c9705c549feb76d
Osma Suominen <osma.suominen@helsinki.fi>
2017-08-24 16:19:22 +0300

    varo aiheuttamasta unicode-virheitä

--- vocabularies/yso-paikat/disambiguate.py
+++ vocabularies/yso-paikat/disambiguate.py
@@ -30,14 +30,14 @@ ambiguous = [(label, places) for label, places in by_label.items() if len(places
 
 for label, places in sorted(ambiguous):
     lang = label.language
-    print >>sys.stderr, "Disambiguating:", label, "lang:", lang, "places:", ' '.join(places)
+    print >>sys.stderr, "Disambiguating:", label.encode('UTF-8'), "lang:", lang, "places:", ' '.join(places)
     for place in places:
         type = get_place_type(g, place, lang)
         if type is None:
             print >>sys.stderr, "-- cannot disambiguate %s" % place
             continue
         newlabel = Literal(u"%s : %s)" % (label[:-1], type), lang)
-        print >>sys.stderr, "-- new label for %s: %s" % (place, newlabel)
+        print >>sys.stderr, ("-- new label for %s: %s" % (place, newlabel)).encode('UTF-8')
         g.remove((place, SKOS.prefLabel, label))
         g.add((place, SKOS.prefLabel, newlabel))
 
