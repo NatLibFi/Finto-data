b6f9ac7d4905fd5e63cf6b08741708026d59dba9
Henri Ylikotila <henri.ylikotila@helsinki.fi>
2017-12-07 17:23:24 +0200

    tietotermit nimiavaruuden vaihto

--- vocabularies/tts/toskos.sh
+++ vocabularies/tts/toskos.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-INFILES="tts-metadata.ttl tieto.ttl"
+INFILES="tt-metadata.ttl tieto.ttl"
 OUTFILE=tieto-skos.ttl
 
 SKOSIFYHOME="../../../Skosify"
 LOGFILE=skosify.log
-OPTS="--namespace http://urn.fi/URN:NBN:fi:au:tts: -c tts.cfg"
+OPTS="--namespace http://urn.fi/URN:NBN:fi:au:tt: -c tt.cfg"
 
 $SKOSIFYHOME/skosify/skosify.py $OPTS $INFILES -o $OUTFILE 2>$LOGFILE
