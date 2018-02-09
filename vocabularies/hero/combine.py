c53c0f43093b95b3aa7c97b8c51ce7d3dc39e3eb
Henri Ylikotila <henri.ylikotila@helsinki.fi>
2018-02-09 15:05:43 +0200

    heron yhdistelytyökalu repoon

---
+++ vocabularies/hero/combine.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+import rdflib, sys
+from rdflib import Graph, RDF, XSD, URIRef, plugin, Literal, Namespace
+import unicodecsv as csv
+from io import BytesIO
+
+skos = Namespace("http://www.w3.org/2004/02/skos/core#")
+herometa = Namespace("http://www.yso.fi/onto/hero-meta/")
+
+hero = Graph().parse('hero.ttl', format='turtle')
+csvfile = open('HERO.csv', 'rb')
+reader = csv.reader(csvfile, encoding='utf-8', delimiter=';')
+types = ['', herometa.UpperConcept, herometa.NonHeraldicConcept, herometa.NonFinnishConcept]
+langcodes = ['et', 'hu', 'en', 'nl', 'de', 'sv', 'nb', 'nn', 'da', 'is', 'lt', 'lv', 'sq', 'el', 'la', 'it', 'fr', 'es', 'ca', 'pt', 'ro', 'pl', 'cs', 'sk', 'hr', 'sr', 'ru', 'be', 'uk']
+
+for row in reader:
+    heroid = row[0]
+    wbo = row[-1]
+    uri = URIRef('http://www.yso.fi/onto/hero/p' + heroid)
+    if (uri, None, None) not in hero:
+        print 'MISSING: ' + heroid 
+    if len(wbo) > 0:
+        hero.add( (uri, herometa.wboMatch, Literal(wbo)) )
+
+    for index, label in enumerate(row[2:-1]):
+        if " {" in label:
+            labstr = label.split(" {")
+            if len(labstr) > 1:
+                pref = labstr[0]
+                lablang = labstr[1][1:3]
+                if lablang == 'fi':
+                    hero.remove((uri, skos.prefLabel, None))
+                hero.add( (uri, skos.prefLabel, Literal(pref, lang=lablang)) )
+        elif len(label) > 0:
+            lablang = langcodes[index]
+            hero.add((uri, skos.prefLabel, Literal(label, lang=lablang)))
+            
+hero.serialize(destination='hero-combined.ttl', format='turtle')
