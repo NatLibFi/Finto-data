2071f17d5534ceb87c11e8705c6dcd7e86a316ac
Henri Ylikotila <henri.ylikotila@helsinki.fi>
2018-02-22 17:08:03 +0200

    korjaillaan heron preffiongelmaa

--- vocabularies/hero/combine.py
+++ vocabularies/hero/combine.py
@@ -12,7 +12,7 @@ hero = Graph().parse('hero.ttl', format='turtle')
 csvfile = open('HERO.csv', 'rb')
 reader = csv.reader(csvfile, encoding='utf-8', delimiter=';')
 types = ['', herometa.UpperConcept, herometa.NonHeraldicConcept, herometa.NonFinnishConcept]
-langcodes = ['et', 'hu', 'en', 'nl', 'de', 'sv', 'nb', 'nn', 'da', 'is', 'lt', 'lv', 'sq', 'el', 'la', 'it', 'fr', 'es', 'ca', 'pt', 'ro', 'pl', 'cs', 'sk', 'hr', 'sr', 'ru', 'be', 'uk']
+langcodes = ['fi', 'et', 'hu', 'en', 'nl', 'de', 'sv', 'nb', 'nn', 'da', 'is', 'lt', 'lv', 'sq', 'el', 'la', 'it', 'fr', 'es', 'ca', 'pt', 'ro', 'pl', 'cs', 'sk', 'hr', 'sr', 'ru', 'be', 'uk']
 wbo_missing = False
 
 for row in reader:
@@ -29,7 +29,7 @@ for row in reader:
     end = -1
     if wbo_missing:
         end = None 
-    for index, label in enumerate(row[2:end]):
+    for index, label in enumerate(row[1:end]):
         if " {" in label:
             labstr = label.split(" {")
             if len(labstr) > 1:
@@ -41,5 +41,7 @@ for row in reader:
         elif len(label) > 0:
             lablang = langcodes[index]
             hero.add((uri, skos.prefLabel, Literal(label, lang=lablang)))
+        if len(label) == 0 and index == 0: # remove false finnish preflabel
+            hero.remove((uri, skos.prefLabel, None))
             
 hero.serialize(destination='hero-combined.ttl', format='turtle')
