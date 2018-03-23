56de455c28b5292845a7e9c5ee9541d3fb4a4234
Osma Suominen <osma.suominen@helsinki.fi>
2018-03-23 11:48:28 +0200

    make sure to preserve old timestamps, even if given empty vocabulary as input

--- tools/timestamper/timestamper.py
+++ tools/timestamper/timestamper.py
@@ -83,17 +83,22 @@ for concept in g.subjects(RDF.type, SKOS.Concept):
             # the concept is new, update timestamp
             new_timestamps[concept] = (hash, timestamp)
 
+# Add the new timestamps to the graph
+for concept, cdata in new_timestamps.items():
+    hash, mtime = cdata
+    if mtime != '-':
+        g.add((concept, DCT.modified, Literal(mtime, datatype=XSD.date)))
+
+# Copy old timestamps that were not in the vocabulary, just in case
+# so we don't lose data if the vocabulary is temporarily missing concepts
+for concept in old_timestamps:
+    if concept not in new_timestamps:
+        new_timestamps[concept] = old_timestamps[concept]
+
 # store the new timestamps in the timestamp data file
 with open(tsfile, 'w') as f:
     for concept, cdata in sorted(new_timestamps.items()):
         hash, mtime = cdata
         print >>f, "\t".join((concept, hash, mtime))
-
-# Add the new timestamps to the graph
-
-for concept, cdata in new_timestamps.items():
-    hash, mtime = cdata
-    if mtime != '-':
-        g.add((concept, DCT.modified, Literal(mtime, datatype=XSD.date)))
 
 g.serialize(destination=sys.stdout, format='turtle')
