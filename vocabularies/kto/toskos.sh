89b5eb78e0af18128b32a4748303e09bfefa8e86
Osma Suominen <osma.suominen@helsinki.fi>
2018-05-15 11:08:47 +0300

    Pyöräytetty KTO uudelleen metatietojen päivityksen takia

--- vocabularies/kto/toskos.sh
+++ vocabularies/kto/toskos.sh
@@ -4,7 +4,7 @@ INFILES="kto-metadata.ttl kto.rdf-xml-ONKIin.owl"
 OUTFILE=kto-skos.ttl
 
 SKOSIFYHOME="../../tools/skosify/"
-CONFFILE=$SKOSIFYHOME/finnonto.cfg
+CONFFILE=../../conf/skosify/finnonto.cfg
 LOGFILE=skosify.log
 
 $SKOSIFYHOME/skosify.py -c $CONFFILE $INFILES -o $OUTFILE 2>$LOGFILE
