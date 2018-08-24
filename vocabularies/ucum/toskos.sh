657f0f9e1b6e8d9037a6992587eabf51d0fb4774
kouralex <1723419+kouralex@users.noreply.github.com>
2018-08-24 09:59:38 +0300

    ucumin päivitys

--- vocabularies/ucum/toskos.sh
+++ vocabularies/ucum/toskos.sh
@@ -5,7 +5,7 @@ OUTFILE=ucum-skos.ttl
 
 SKOSIFYHOME="../../tools/skosify/"
 LOGFILE=skosify.log
-#OPTS="--namespace http://www.yso.fi/onto/ucum/"
+OPTS="--namespace http://urn.fi/URN:NBN:fi:au:ucum: --mark-top-concepts"
 #-c udcs.cfg
 
 $SKOSIFYHOME/skosify.py $OPTS $INFILES -o $OUTFILE 2>$LOGFILE
