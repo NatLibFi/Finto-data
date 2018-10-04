a582e44a495beb26cc9b99f273ba2c300ccfd914
Joeli Takala <joeli.takala@helsinki.fi>
2018-10-04 15:35:38 +0300

    UDK:n päivitetty versio

--- vocabularies/udcs/toskos.sh
+++ vocabularies/udcs/toskos.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-INFILES="udcs-metadata.ttl udc-scheme.rdf udcsummary-skos.rdf"
+INFILES="udcs-metadata.ttl udc-scheme.rdf udcsummary-skos.ttl"
 OUTFILE=udcs-skos.ttl
 
 SKOSIFYHOME="../../tools/skosify/"
