f03a2b1faa0b3f4719ee658b03b0c87bc4dee190
Joeli Takala <joeli.takala@helsinki.fi>
2018-12-21 14:11:32 +0200

    YKL viety Fintoon

---
+++ vocabularies/ykl/toskos.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+INFILES="ykl-metadata.ttl ykl.ttl"
+OUTFILE=ykl-skos.ttl
+
+SKOSIFYHOME="../../tools/skosify/"
+LOGFILE=skosify.log
+OPTS="--namespace http://urn.fi/URN:NBN:fi:au:ykl: --mark-top-concepts"
+
+
+$SKOSIFYHOME/skosify.py $OPTS $INFILES -o $OUTFILE 2>$LOGFILE
