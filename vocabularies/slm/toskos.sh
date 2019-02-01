7920634017da7d81786e5a09001d39deb24ce75b
Osma Suominen <osma.suominen@helsinki.fi>
2019-02-01 16:36:52 +0200

    force turtle output so timestamper can parse it

--- vocabularies/slm/toskos.sh
+++ vocabularies/slm/toskos.sh
@@ -7,6 +7,6 @@ SKOSIFYHOME="../../../Skosify"
 TIMESTAMPER="../../tools/timestamper/timestamper.py"
 LOGFILE=skosify.log
 TIMESTAMPFILE=timestamps.tsv
-OPTS="--namespace http://urn.fi/URN:NBN:fi:au:mts: -c ./slm.cfg"
+OPTS="--namespace http://urn.fi/URN:NBN:fi:au:mts: -c ./slm.cfg -F turtle"
 
 $SKOSIFYHOME/skosify/skosify.py $OPTS $INFILES 2>$LOGFILE | $TIMESTAMPER $TIMESTAMPFILE >$OUTFILE
