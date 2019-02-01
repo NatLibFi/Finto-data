6c842eb2ff1ef37e090424793db6f6621cbc1d9e
Osma Suominen <osma.suominen@helsinki.fi>
2019-02-01 16:34:25 +0200

    Run timestamper for SLM

--- vocabularies/slm/toskos.sh
+++ vocabularies/slm/toskos.sh
@@ -4,7 +4,9 @@ INFILES="slm.ttl"
 OUTFILE=slm-skos.ttl
 
 SKOSIFYHOME="../../../Skosify"
+TIMESTAMPER="../../tools/timestamper/timestamper.py"
 LOGFILE=skosify.log
+TIMESTAMPFILE=timestamps.tsv
 OPTS="--namespace http://urn.fi/URN:NBN:fi:au:mts: -c ./slm.cfg"
 
-$SKOSIFYHOME/skosify/skosify.py $OPTS $INFILES -o $OUTFILE 2>$LOGFILE
+$SKOSIFYHOME/skosify/skosify.py $OPTS $INFILES 2>$LOGFILE | $TIMESTAMPER $TIMESTAMPFILE >$OUTFILE
