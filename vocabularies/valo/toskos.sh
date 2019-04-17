60bb8657a76a844d104439af13ec5f4f86820700
Joeli Takala <joeli.takala@helsinki.fi>
2019-04-17 12:33:30 +0300

    Muutama käsite päivitetty VALO:ssa

--- vocabularies/valo/toskos.sh
+++ vocabularies/valo/toskos.sh
@@ -3,8 +3,8 @@ cp ../yso/ysoKehitys.rdf yso-import.rdf
 INFILES="valo-metadata.ttl valo.ttl yso-import.rdf"
 OUTFILE=valo-skos.ttl
 
-SKOSIFYHOME="../../../Skosify/"
+SKOSIFYHOME="../../tools/skosify/"
 CONFFILE=$SKOSIFYHOME/finnonto.cfg
 LOGFILE=skosify.log
 
-$SKOSIFYHOME/skosify/skosify.py -c $CONFFILE $INFILES -o $OUTFILE 2>$LOGFILE
+$SKOSIFYHOME//skosify.py -c $CONFFILE $INFILES -o $OUTFILE 2>$LOGFILE
