d5752c603feecae91cc404323398146043f48f40
Joeli Takala <joeli.takala@helsinki.fi>
2019-11-01 09:03:27 +0200

    KAUNO päivitetty

--- vocabularies/kauno/toskos.sh
+++ vocabularies/kauno/toskos.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 #cp ../yso/ysoKehitys.rdf yso-import.rdf
-INFILES="kauno-metadata.ttl kauno.ttl yso-import.rdf"
+cp ../yso/releases/2019.3.Cicero/ysoKehitys.rdf ./yso-cicero.rdf
+INFILES="kauno-metadata.ttl kauno.ttl yso-cicero.rdf"
 OUTFILE=kauno-skos.ttl
 
-SKOSIFYHOME="../../tools/Skosify/skosify"
+SKOSIFYHOME="../../tools/skosify"
 CONFFILE="../../conf/skosify/finnonto.cfg"
 LOGFILE=skosify.log
 
