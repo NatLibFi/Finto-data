c9113c5710a64aa1bdf02b811b579e0a57639f1e
Joeli Takala <joeli.takala@helsinki.fi>
2020-02-13 16:23:24 +0200

    tero päivitetty

--- vocabularies/tero/toskos.sh
+++ vocabularies/tero/toskos.sh
@@ -3,7 +3,7 @@
 INFILES="tero-metadata.ttl tero-yso-replacedby.ttl tero-missing-tero-types.ttl tero.ttl yso-tero.ttl"
 OUTFILE=tero-skos.ttl
 
-SKOSIFYHOME="../../tools/Skosify"
+SKOSIFYHOME="../../tools/skosify"
 LOGFILE=skosify.log
 #OPTS="-c tero.cfg -l fi -f turtle"
 OPTS="-c ../../conf/skosify/finnonto.cfg -l fi -f turtle"
