ef332d5bd521aecaf81cb3598a8f9dff3c3352ee
kouralex <1723419+kouralex@users.noreply.github.com>
2020-02-25 14:13:34 +0200

    päivitetty expand-note-uris-skriptiä

--- tools/expand-note-uris/expand-note-uris.py
+++ tools/expand-note-uris/expand-note-uris.py
@@ -3,25 +3,35 @@
 # Expand references to concept URIs to HTML links with concept labels
 # in scope notes and definitions. Write the modified vocabulary on stdout.
 
+from __future__ import print_function
 from rdflib import Graph, URIRef, Literal
 from rdflib.namespace import SKOS
 import re
 import sys
 
 inputfile = sys.argv[1]
+URIRE = re.compile(r'\[http([^\]]+)\]')
 
 g = Graph()
 g.parse(inputfile, format='turtle')
 
-URIRE = re.compile(r'\[(http://www.yso.fi/onto/[a-z]+/p[0-9]+)\]')
-
-def uri_to_link(lang, matchobj):
-    uri = matchobj.group(1)
+def uri_to_link(sub, prop, lang, matchobj):
+    uri = 'http' + matchobj.group(1)
     concept = URIRef(uri)
     labels = g.preferredLabel(concept, lang)
     try:
         label = labels[0][1]
     except IndexError:
+        try:
+            g2 = Graph()
+            g2.parse(uri)
+            g2.preferredLabel(concept, lang)
+            labels = g2.preferredLabel(concept, lang)
+            label = labels[0][1]
+            return "<a href='%s'>%s</a>" % (uri, label)
+        except:
+            print("Concept %s: could not retrieve label for '%s' found in %s in language %s" % (sub, concept, prop, lang), file=sys.stderr)
+            return matchobj.group(0) # don't change if we can't find a label
         return matchobj.group(0) # don't change if we can't find a label
     return "<a href='%s'>%s</a>" % (uri, label)
     
@@ -32,7 +42,7 @@ for prop in NOTEPROPS:
     for s,o in g.subject_objects(prop):
         lang = o.language
         if URIRE.search(o) is not None:
-            new = URIRE.sub(lambda m:uri_to_link(lang, m), o)
+            new = URIRE.sub(lambda m:uri_to_link(s, prop, lang, m), o)
             g.remove((s, prop, o))
             g.add((s, prop, Literal(new, lang)))
             
