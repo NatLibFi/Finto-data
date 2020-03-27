a3da3505f9b65d4e366ada844f2b78fb5dc362df
Finto Machine User <Finto-data@users.noreply.github.com>
2020-03-27 13:27:37 +0200

    yso-paikat oli jäänyt checksum mismatchiin

--- vocabularies/yso-paikat/vb-toskos.sh
+++ vocabularies/yso-paikat/vb-toskos.sh
@@ -4,11 +4,13 @@
 rsparql --results NT --service https://query.wikidata.org/sparql --query wikidata-links.rq | sort >wikidata-links.nt
 
 INFILES="yso-paikat-metadata.ttl yso-paikat-vb-dump.rdf wikidata-links.nt"
-OUTFILE=yso-paikat-vb-skos.ttl
-CFGFILE=ysopaikat-vb.cfg
+OUTFILE=yso-paikat-skos.ttl
+CFGFILE=yso-paikat-vb.cfg
+#CFGFILE=../../conf/skosify/finnonto.cfg
 
-SKOSIFYHOME="../../tools/skosify/"
+
+SKOSIFYHOME="../../tools/skosify"
 LOGFILE=skosify.log
 OPTS="--no-enrich-mappings --set-modified --namespace http://www.yso.fi/onto/yso/"
 
-$SKOSIFYHOME/skosify.py -c $CFGFILE $INFILES -o $OUTFILE 2>$LOGFILE
+$SKOSIFYHOME/skosify.py -c $CFGFILE $OPTS $INFILES -o $OUTFILE 2>$LOGFILE
