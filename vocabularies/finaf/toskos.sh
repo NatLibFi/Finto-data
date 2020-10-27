d9ab847680a33b02da44e30dd347fb27de25667e
Osma Suominen <osma.suominen@helsinki.fi>
2020-10-27 11:30:01 +0200

    Rerun finaf conversion. Use latest skosify.

--- vocabularies/finaf/toskos.sh
+++ vocabularies/finaf/toskos.sh
@@ -3,8 +3,8 @@
 INFILES="finaf-metadata.ttl rdaa.rdf rdac.rdf rdap.rdf rdau.rdf finaf.ttl"
 OUTFILE=finaf-skos.ttl
 
-SKOSIFYHOME="../../tools/skosify"
+SKOSIFYCMD="skosify"
 LOGFILE=skosify.log
 OPTS="--set-modified --no-mark-top-concepts --no-enrich-mappings --namespace http://urn.fi/URN:NBN:fi:au:finaf:"
 
-$SKOSIFYHOME/skosify.py $OPTS $INFILES -o $OUTFILE 2>$LOGFILE
+$SKOSIFYCMD $OPTS $INFILES -o $OUTFILE 2>$LOGFILE
