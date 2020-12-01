66c4c17b6de1c80915b6f94cb4283847f60a69dd
kouralex <1723419+kouralex@users.noreply.github.com>
2020-12-01 13:27:27 +0200

    päivitetään HERO

--- vocabularies/hero/toskos.sh
+++ vocabularies/hero/toskos.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-INFILES="hero-combined.ttl"
+INFILES="hero.ttl"
+#INFILES="hero-combined.ttl"
 OUTFILE=hero-skos.ttl
 
-SKOSIFYHOME="../../tools/skosify/"
 LOGFILE=skosify.log
 OPTS="--namespace http://www.yso.fi/onto/hero/ -c ../../conf/skosify/finnonto.cfg"
 
-$SKOSIFYHOME/skosify.py $OPTS $INFILES -o $OUTFILE 2>$LOGFILE
+skosify $OPTS $INFILES -o $OUTFILE 2>$LOGFILE
