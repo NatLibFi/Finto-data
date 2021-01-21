057ba8af9f86423467c4d78646c6309d366647d9
kouralex <1723419+kouralex@users.noreply.github.com>
2021-01-21 15:52:14 +0200

    päivitysehdotus ykl:lle

--- vocabularies/ykl/toskos.sh
+++ vocabularies/ykl/toskos.sh
@@ -1,11 +1,21 @@
 #!/bin/sh
 
-INFILES="ykl-metadata.ttl ykl.ttl ykl-hklj.ttl"
+# step 1: remove manually unwanted extra triples from the rawfile (e.g., <file://...)
+
+# step 2: set $RAWFILE to be the file processed in step 1
+
+RAWFILE="ykl.ttl"
+PROCESSEDFILE="ykl-processed.ttl"
+
+SKOSIFYCMD="skosify"
+
+# remove (excess) whitespaces before, between (in long quoted literals) and after in literals
+cat $RAWFILE | sed 's/[ ]\+/ /g' | sed 's/  //g' | sed -r 's/ \+[)]/\+\)/g' | sed -r 's/([0-9]) \+([^ ][^0-9])/\1\+\2/g' | sed 's/ *$//' | sed 's/^ *//' > $PROCESSEDFILE
+
+INFILES="ykl-metadata.ttl $PROCESSEDFILE ykl-hklj.ttl"
 OUTFILE=ykl-skos.ttl
 
-SKOSIFYHOME="../../tools/skosify/"
 LOGFILE=skosify.log
-OPTS="--namespace http://urn.fi/URN:NBN:fi:au:ykl: --mark-top-concepts"
+OPTS="-c ykl.cfg --namespace http://urn.fi/URN:NBN:fi:au:ykl: --mark-top-concepts"
 
-
-$SKOSIFYHOME/skosify.py $OPTS $INFILES -o $OUTFILE 2>$LOGFILE
+$SKOSIFYCMD $OPTS $INFILES -o $OUTFILE 2>$LOGFILE
