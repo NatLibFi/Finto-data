99041fbb0e49013de4300022d7bf84d52f5f3216
Ahonen Mika J <mika.ahonen@helsinki.fi>
2021-03-17 16:37:07 +0200

    Juho updated and new concepts added

--- vocabularies/juho/purify.sh
+++ vocabularies/juho/purify.sh
@@ -13,6 +13,8 @@ OUTFILE="${ONTOFILE}.new"
 $PURIFY -c $CONTEXT -s $PURICHAR -f $FORMAT -t $FORMAT $ONTOFILE $ONTONS $ONTOPURIBASE >$OUTFILE
 if [ -s $OUTFILE ]; then
   mv $OUTFILE $ONTOFILE
+  sleep 5
+  wget -O puri-history.log http://puri.onki.fi/getmappings?context=$CONTEXT 2> /dev/null
   echo "Done, replaced $ONTOFILE with purified version."
   echo "Remember to load and re-save using TBC to preserve file order!"
 fi
--- vocabularies/juho/toskos.sh
+++ vocabularies/juho/toskos.sh
@@ -1,14 +1,12 @@
 #!/bin/sh
 
-cp ../yso/releases/2020.1.Diotima/ysoKehitys.rdf yso-import.rdf
-
 INFILE="juho.ttl"
-INFILES="juho-metadata.ttl yso-import.rdf $INFILE"
+INFILES="juho-metadata.ttl juho-singular.ttl juhoDeprecated.ttl recoveredDeletedConcepts.ttl ../yso/releases/2020.1.Diotima/ysoKehitys.rdf $INFILE"
 OUTFILE=juho-skos.ttl
 
-SKOSIFYHOME="../../../Skosify/"
+SKOSIFYCMD=skosify
 CONFFILE=../../conf/skosify/finnonto.cfg
 LOGFILE=skosify.log
 OPTS="-l fi"
 
-$SKOSIFYHOME/skosify.py $OPTS -c $CONFFILE -o $OUTFILE $INFILES 2>$LOGFILE
+$SKOSIFYCMD $OPTS -c $CONFFILE -o $OUTFILE $INFILES 2>$LOGFILE
