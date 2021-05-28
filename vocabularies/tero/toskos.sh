bdc5d0fb0abd06d9a56bfd2577c96d61f83ed70b
kouralex <1723419+kouralex@users.noreply.github.com>
2021-05-28 20:45:55 +0300

    päivitetään TEROn metatiedot; korjataan samalla merkkijonoliteraalien duplikaattimääritys

--- vocabularies/tero/toskos.sh
+++ vocabularies/tero/toskos.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
-INFILES="tero-metadata.ttl tero-yso-replacedby.ttl tero.ttl yso-tero.ttl"
+# temporary file for uniforming the literal serialization of explicitly stated xsd:string datatypes with implicit ones
+YSOTEROMODIFIED=yso-tero-modified.ttl
+cat yso-tero.ttl | sed -e 's/"^^xsd:string/"/g' > $YSOTEROMODIFIED
+
+INFILES="tero-metadata.ttl tero-yso-replacedby.ttl tero.ttl $YSOTEROMODIFIED"
 OUTFILE=tero-skos.ttl
 
 LOGFILE=skosify.log
@@ -8,3 +12,6 @@ LOGFILE=skosify.log
 OPTS="-c ../../conf/skosify/finnonto.cfg -l fi -f turtle"
 
 ./add-tero-types.py $INFILES | skosify $OPTS - -o $OUTFILE 2>$LOGFILE
+
+# remove the temporary file afterwards
+rm $YSOTEROMODIFIED
