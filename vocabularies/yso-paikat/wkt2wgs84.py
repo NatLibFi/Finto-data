e2ddde023f62ac4cc0befe6f1f99bc1d37c38b9c
Vainonen <okvainon@ad.helsinki.fi>
2021-09-02 16:11:13 +0300

    add exception handling for malformatted coordinates

--- vocabularies/yso-paikat/wkt2wgs84.py
+++ vocabularies/yso-paikat/wkt2wgs84.py
@@ -4,6 +4,15 @@
 import argparse
 import sys
 
+
+def printCoordinates(listing, current):
+    point = sorted(listing, key=len)[-1].split()
+    try:
+        print(current + ' wgs84:long "' + "{:.5f}".format(float(point[0][7:])).rstrip('0').rstrip('.') + '" .')
+        print(current + ' wgs84:lat "' + "{:.5f}".format(float(point[1][:-53])).rstrip('0').rstrip('.') + '" .')
+    except (ValueError, IndexError):
+        print ("Warning: '%s' has malformatted coordinates" % (current), file=sys.stderr)
+
 def readCommandLineArguments():
     parser = argparse.ArgumentParser(description="Program for generating single value coordinates out of <http://www.wikidata.org/prop/direct/P625> predicate triple.")
     parser.add_argument("-i", "--input", nargs='?', type=argparse.FileType('r'), help="Input file location, e.g., wikidata-links.nt. Expects a sorted .nt file.", default=sys.stdin)
@@ -15,7 +24,6 @@ def readCommandLineArguments():
 def main():
         args = readCommandLineArguments()
         sys.stdout = args.output
-
         print("@prefix wgs84: <http://www.w3.org/2003/01/geo/wgs84_pos#> .")
 
         current = None
@@ -24,9 +32,7 @@ def main():
             split = row.split()
             if current != split[0]:
                 if len(listing):
-                    point = sorted(listing, key=len)[-1].split()
-                    print(current + ' wgs84:long "' + "{:.5f}".format(float(point[0][7:])).rstrip('0').rstrip('.') + '" .')
-                    print(current + ' wgs84:lat "' + "{:.5f}".format(float(point[1][:-53])).rstrip('0').rstrip('.') + '" .')
+                    printCoordinates(listing, current)
                 listing = []
 
             current = split[0]
@@ -35,10 +41,7 @@ def main():
             else:
                 print(row)
         if len(listing):
-            point = sorted(listing, key=len)[-1].split()
-            print(current + ' wgs84:long "' + "{:.5f}".format(float(point[0][7:])).rstrip('0').rstrip('.') + '" .')
-            print(current + ' wgs84:lat "' + "{:.5f}".format(float(point[1][:-53])).rstrip('0').rstrip('.') + '" .')
-
+            printCoordinates(listing, current)
 
 if __name__ == "__main__":
     main()
