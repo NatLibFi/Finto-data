d2c87b463d8e629b2f33798fc7e28f1000e25d72
Mika Vaara <mika.vaara@helsinki.fi>
2022-02-18 14:53:53 +0200

    Use rdf-to-ttl.py if you need to convert rdf to ttl. Cannot be used for other purposes than mutu process (does not take conventional prefixes into account, changes them like ns: ..).

---
+++ tools/mutu-helpers/rdf-to-ttl.py
@@ -0,0 +1,27 @@
+#!peekolmonen/bin/python3
+#/usr/bin/python3
+# coding=utf-8
+# You may need to install rdflib. If so, run pip install rdflib. 
+# If you are working in a Python virtual environment:
+# run pip3 install rdflib
+# Run the script this way:
+# ./rdf-to-ttl.py [source-folder]/file.rdf [target-folder]/file.ttl
+
+import sys, rdflib
+
+g = rdflib.Graph()
+g_target = rdflib.Graph()
+g.parse(open(sys.argv[1], "r"), format="xml")
+file_to_be_saved = open(sys.argv[2], "w+")
+
+for s, p, o in g.triples((None, None, None)):
+    g_target.add((s, p, o))
+
+print("Amount of triples in the original")
+print(len(g))
+print("Amount of triples in the target")
+print(len(g_target))
+
+file_to_be_saved.write(g_target.serialize(format="turtle"))
+file_to_be_saved.close()
+
