336be5f55ee160a9151a3016ce2fa0efad34b758
Mika Vaara <mika.vaara@helsinki.fi>
2022-04-14 14:59:29 +0300

    Uusi skripti GitHubissa suljettujen issueiden ja YSEssä olevien ehdotusten labelien vertailemiseksi ja poistamiseksi. Voidaan ottaa käyttöön, jos todetaan tuotannossa toimivaksi ja varmuuskopioitu aiempi skripti voidaan silloin poistaa

--- tools/yse-updater/check-closed-issues.py
+++ tools/yse-updater/check-closed-issues.py
@@ -1,58 +1,55 @@
 #!/usr/bin/python3
 # coding=utf-8
-import urllib.request, urllib.parse, urllib.error, sys, pickle, rdflib, json
-import requests, time, calendar, datetime, sys
-from rdflib import Graph, Namespace,RDF ,XSD ,OWL , URIRef, plugin, Literal
+from rdflib import Graph, Namespace, URIRef
 from github import Github
-from urllib.parse import urlencode
+import sys, json, datetime
 
-ysans = "http://www.yso.fi/onto/ysa/"
-skosns = "http://www.w3.org/2004/02/skos/core#"
-newtriples = Graph()
 skos = Namespace("http://www.w3.org/2004/02/skos/core#")
-dct = Namespace('http://purl.org/dc/terms/')
-isothes = Namespace('http://purl.org/iso25964/skos-thes#')
-foaf = Namespace('http://xmlns.com/foaf/0.1/')
-ysa = Namespace('http://www.yso.fi/onto/ysa/')
 
 if len(sys.argv) < 3:
     print("Usage: %s GitHub-credentials-file Finto-data-path" % sys.argv[0], file=sys.stderr)
     sys.exit()
+else:
+  print("Starting to check matching labels between YSE and closed GitHub issues, please wait..\n")
 
-secrets = json.load(open(sys.argv[1]))
-finto_data = sys.argv[2]
-ysa_file = finto_data + '/vocabularies/ysa/ysa-skos.ttl'
-yse_file = finto_data + '/vocabularies/yse/yse-skos.ttl'
-
-gh = Github(secrets['username'], secrets['password'])
-repo = gh.get_user('Finto-ehdotus').get_repo('YSE')
-label = repo.get_label('uusi')
-need_inspection = repo.get_issues(state='closed', labels=[label])
-ysa_skos = Graph().parse(ysa_file, format='turtle')
+yse_file = sys.argv[2] + '/vocabularies/yse/yse-skos.ttl'
+# yse_file = sys.argv[2] + '/yse-skos.ttl' # only for testing purposes
 yse_skos = Graph().parse(yse_file, format='turtle')
 
-def delete_triples(uri):
-    yse_skos.remove((URIRef(uri), None, None))
-    yse_skos.remove((None, None, URIRef(uri)))
+yse_triples = []
+preflabels_in_yse = 0
+for triple in yse_skos.triples((None, skos['prefLabel'], None)):
+    preflabels_in_yse += 1
+    yse_triples.append(triple)
 
-def replaced_by(uri, title):
-    new_uri = lookup(title)
-    if(new_uri != ''):
-        yse_skos.remove((None, None, URIRef(uri)))
-        yse_skos.add((URIRef(uri), dct.isReplacedBy, URIRef(new_uri)))
-        yse_skos.add((URIRef(uri), OWL.deprecated, Literal('true',datatype=XSD.boolean)))
+secrets = json.load(open(sys.argv[1]))
+gh = Github(secrets['username'], secrets['password'])
+repo = gh.get_user('Finto-ehdotus').get_repo('YSE')
+need_inspection = repo.get_issues(state='closed')
 
+gh_titles = []
 for issue in need_inspection:
-    if ('**Termiehdotus Fintossa:**' in issue.body):
-        md_href = issue.body.split("**Termiehdotus Fintossa:**")[1]
-        if md_href:
-            suggestion_uri = ysa + md_href.split('page/')[1].replace(')', '')
-            ysalab = ysa_skos.preferredLabel(suggestion_uri, lang='fi')
-            yselab = yse_skos.preferredLabel(suggestion_uri, lang='fi')
-            if yselab != '' and ysalab == yselab: # the suggestion has been taken into YSA with the same prefLabel
-                print("deleting: " + issue.title)
-                delete_triples(suggestion_uri)
-            else:
-                print(('add replacedBy to:' + suggestion_uri))
+    gh_titles.append(issue.title)
+
+yse_triples_set = set(yse_triples)
+gh_titles_stripped = [i.strip() for i in gh_titles]
+gh_titles_set = set(gh_titles_stripped)
+
+how_many_tripes_to_remove = 0
+for triple in yse_triples_set:
+    if triple[2].strip() in gh_titles_set:
+      print("* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *")
+      print(f'> The suggestion with the uri {triple[0]} has been taken into YSO\r')
+      print(f'> Matching prefLabel or altLabel is: {triple[2]}\r')
+      yse_skos.remove((URIRef(triple[0]), None, None))
+      yse_skos.remove((None, None, URIRef(triple[0])))
+      print(f'> "{triple[2]}" removed\n')
+      how_many_tripes_to_remove += 1
+
+print(datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
+print(f'Closed issues total: {len(gh_titles_set)}')
+print(f'prefLabels in YSE: {preflabels_in_yse}')
+print(f'Amount of suggestions to be removed: {how_many_tripes_to_remove}')
 
 yse_skos.serialize(destination=yse_file, format='turtle')
+
