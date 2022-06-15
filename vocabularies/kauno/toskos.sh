d539a10cba1271633e8df29639fac7563588926b
Joeli Takala <joeli.takala@helsinki.fi>
2022-06-15 09:26:28 +0300

    KAUNO päivitetty

--- vocabularies/kauno/toskos.sh
+++ vocabularies/kauno/toskos.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 INFILE="kauno.ttl"
-INFILES="kauno-metadata.ttl ../yso/releases/2019.3.Cicero/ysoKehitys.rdf $INFILE"
+INFILES="kauno-metadata.ttl ../yso/releases/2022.1.Filolaos/ysoKehitys.rdf $INFILE"
 OUTFILE=kauno-skos.ttl
 
 SKOSIFYCMD="skosify"
