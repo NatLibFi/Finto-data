45616bac19872b50acd19bb4e367b316f963de4e
Mika Vaara <mika.vaara@helsinki.fi>
2022-06-22 11:24:53 +0300

    Siirrytty käyttämään yhteistä finnonto.cfg-tiedostoa OMAlle

--- vocabularies/oma/toskos.sh
+++ vocabularies/oma/toskos.sh
@@ -4,7 +4,7 @@ INFILES="mehi.ttl ../yso/releases/2021.3.Epikuros/ysoKehitys.rdf"
 OUTFILE=oma-skos.ttl
 
 SKOSIFYCMD="skosify"
-CONFFILE=./finnonto.cfg
+CONFFILE=../../conf/skosify/finnonto.cfg
 LOGFILE=skosify.log
 
 $SKOSIFYCMD -c $CONFFILE $INFILES -o $OUTFILE 2>$LOGFILE
