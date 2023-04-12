9da96d297dcb517b59dcbef51aedd2f6114e9dc8
Finto-data <finto-posti@helsinki.fi>
2023-04-12 16:06:37 +0300

    Uusi ja parempi yso-paikkojen purituskomentosarja

--- vocabularies/yso-paikat/purify.sh
+++ vocabularies/yso-paikat/purify.sh
@@ -7,27 +7,16 @@ PURIFY=../../tools/purify/purify.py
 PAIKATNS=http://www.yso.fi/onto/yso/
 PURICHAR=p
 PAIKATPURIBASE="${PAIKATNS}${PURICHAR}"
-CONTEXT=$PAIKATPURIBASE
+CONTEXT=http://www.yso.fi/onto/yso-paikat/
 OUTFILE="${PAIKATFILE}.new"
 
-# Saving the current YSO maxcounter from puri.onki.fi, as we are about to use YSO-paikat counter range
-COUNTER=$(curl -s -H "Pragma: no-cache" "http://puri.onki.fi/getcounter?purins=http%3A%2F%2Fwww.yso.fi%2Fonto%2Fyso%2Fp")
 
-if [ $COUNTER -lt 100000 ]; then
+# '-C auto' parameter is not a documented feature of purify.py - it is needed for setting the yso-paikat puri number range of 100000+
+$PURIFY -c $CONTEXT -s $PURICHAR -f $FORMAT -t $FORMAT -C auto $PAIKATFILE $PAIKATNS $PAIKATPURIBASE >$OUTFILE
 
-  # '-C auto' parameter is not a documented feature of purify.py - it is needed for setting the yso-paikat puri number range of 100000+
-  $PURIFY -c $CONTEXT -s $PURICHAR -f $FORMAT -t $FORMAT -C auto $PAIKATFILE $PAIKATNS $PAIKATPURIBASE >$OUTFILE
-  sed -i 's/:p508540/:places/g' $OUTFILE # Prevent purify from changing the conceptScheme
+sed -i 's/:p508722/:places/g' $OUTFILE # Prevent purify from changing the conceptScheme
 
-  if [ -s $OUTFILE ]; then
-    mv -f $OUTFILE $PAIKATFILE
-    echo "Done, replaced $PAIKATFILE with purified version."
-  fi
-
-  #Setting the old maxcounter back not to mess with YSOs puri counter
-  curl "http://puri.onki.fi/setcounter?purins=http%3A%2F%2Fwww.yso.fi%2Fonto%2Fyso%2Fp&counter=$COUNTER"
-
-  else
-    echo "puri.onki.fi counter not in the excepted range for YSO-puris ( < 100000)"
-    echo -e "puri.onki.fi counter not in the excepted range for YSO-puris ( < 100000)" | mail -s "Ongelma YSO-paikoissa" joeli.takala@helsinki.fi
+if [ -s $OUTFILE ]; then
+  mv -f $OUTFILE $PAIKATFILE
+  echo "Done, replaced $PAIKATFILE with purified version."
 fi
