d8d812bd00c03d0aff8a363f66d83ee16a25a8a0
Vainonen <8686308+Vainonen@users.noreply.github.com>
2023-04-14 14:25:55 +0300

    Päivitetty MML:n koordinaatit ja paikkatyypit

--- vocabularies/yso-paikat/toskos.sh
+++ vocabularies/yso-paikat/toskos.sh
@@ -12,7 +12,7 @@ rsparql --results NT --service https://query.wikidata.org/sparql --query wikidat
 grep -oP "(?<=rdf:resource=\"http://paikkatiedot.fi/so/1000772/).*(?=\")" yso-paikat-vb-dump.ttl | sort -u > yso-paikat-usedPNRs.txt
 
 # create wgs84 lat/long literals for used PNR places
-./extractUsedPNRs.py --input pnr-complete-paikkaid-wgs84-coordinates-table-2022-10-21.csv --selector yso-paikat-usedPNRs.txt > yso-paikat-pnr.ttl
+./extractUsedPNRs.py --input pnr-complete-paikkaid-wgs84-coordinates-table-2023-04-14.csv --selector yso-paikat-usedPNRs.txt > yso-paikat-pnr.ttl
 
 # expand URIs in notes and definitions
 $EXPANDURIS yso-paikat-vb-dump.ttl >yso-paikat-expanded.ttl 2>yso-paikat-expanded.log
