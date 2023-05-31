b49c1d81588eff08f15cafc7a910644b6c291ddd
Osma Suominen <osma.suominen@helsinki.fi>
2023-05-31 17:34:57 +0300

    Use purify-local.py for YSO-paikat instead of purify.py

--- vocabularies/yso-paikat/purify.sh
+++ vocabularies/yso-paikat/purify.sh
@@ -3,16 +3,16 @@
 PAIKATFILE=yso-paikat-vb-dump.ttl
 FORMAT=turtle
 
-PURIFY=../../tools/purify/purify.py
+PURIFY=../../tools/purify/purify-local.py
 PAIKATNS=http://www.yso.fi/onto/yso/
 PURICHAR=p
 PAIKATPURIBASE="${PAIKATNS}${PURICHAR}"
-CONTEXT=http://www.yso.fi/onto/yso-paikat/
+PURIFILE=puri-mappings.tsv
 OUTFILE="${PAIKATFILE}.new"
 
 
 # '-C auto' parameter is not a documented feature of purify.py - it is needed for setting the yso-paikat puri number range of 100000+
-$PURIFY -c $CONTEXT -s $PURICHAR -f $FORMAT -t $FORMAT -C auto $PAIKATFILE $PAIKATNS $PAIKATPURIBASE >$OUTFILE
+$PURIFY -s $PURICHAR -f $FORMAT -t $FORMAT -C auto $PAIKATFILE $PAIKATNS $PAIKATPURIBASE $PURIFILE >$OUTFILE
 
 sed -i 's/:p508722/:places/g' $OUTFILE # Prevent purify from changing the conceptScheme
 
