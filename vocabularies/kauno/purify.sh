f2c955421c13fc9a9bfb82511af4d12922b31994
Joeli Takala <joeli.takala@helsinki.fi>
2024-01-17 11:14:47 +0200

    KAUNOn puritusta korjattu, paluu osin 10/2023 edeltäviin tunnuksiin

--- vocabularies/kauno/purify.sh
+++ vocabularies/kauno/purify.sh
@@ -3,17 +3,17 @@
 ONTOFILE=kauno.ttl
 FORMAT=turtle
 
-PURIFY=../../tools/purify/purify.py
+PURIFY=../../tools/purify/purify-local.py
 ONTONS=http://www.yso.fi/onto/kauno/
 PURICHAR=p
 ONTOPURIBASE="${ONTONS}${PURICHAR}"
+PURIMAPPINGS=puri-mappings.tsv
 CONTEXT=$ONTOPURIBASE
 OUTFILE="${ONTOFILE}.new"
 
-$PURIFY -c $CONTEXT -s $PURICHAR -f $FORMAT -t $FORMAT $ONTOFILE $ONTONS $ONTOPURIBASE >$OUTFILE
+$PURIFY -s $PURICHAR -f $FORMAT -t $FORMAT $ONTOFILE $ONTONS $ONTOPURIBASE $PURIMAPPINGS >$OUTFILE
 if [ -s $OUTFILE ]; then
   mv $OUTFILE $ONTOFILE
   sleep 5
-  wget -O puri-history.log http://puri.onki.fi/getmappings?context=$CONTEXT 2> /dev/null
   echo "Done, replaced $ONTOFILE with purified version."
 fi
