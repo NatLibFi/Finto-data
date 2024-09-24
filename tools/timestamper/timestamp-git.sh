39ca6f920a9597397a87011352cb5e23d25c5c13
Osma Suominen <osma.suominen@helsinki.fi>
2024-09-24 13:53:02 +0300

    fix git commands; only run timestamper.py when the vocab file has changed

--- tools/timestamper/timestamp-git.sh
+++ tools/timestamper/timestamp-git.sh
@@ -9,12 +9,22 @@ earliest=$4
 # date range
 latest=`date '+%Y-%m-%d'`
 
+# last commit id
+last_commit_id=""
+
 echo "Loading $vocabfile snapshots ($earliest to $latest) from $gitdir"
 
 function load_from_git() {
 	gitdir=$1
 	date=$2
-	git --git-dir "${gitdir}/.git" --work-tree "$gitdir" show @{$date}:$vocabfile | ./timestamper.py $tsfile $date >/dev/null
+	commit_id=`git --git-dir "${gitdir}/.git" --work-tree "$gitdir" log --before="$date" -n 1 --format="%H" -- $gitdir/$vocabfile`
+	if [ "$commit_id" != "$last_commit_id" ]; then
+		echo "- commit $commit_id different from $last_commit_id, running timestamper"
+		git --git-dir "${gitdir}/.git" --work-tree "$gitdir" show $commit_id:$vocabfile | ./timestamper.py $tsfile $date >/dev/null
+		last_commit_id=$commit_id
+	else
+		echo "- commit $commit_id unchanged, skipping timestamper"
+	fi
 }
 
 for year in $(seq 2014 `date '+%Y'`); do
