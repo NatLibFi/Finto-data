b5acee02fd57bf4c3312e7f583497247d80a8855
Joeli Takala <joeli.takala@helsinki.fi>
2025-01-15 11:24:20 +0200

    MAOTAO päivitetty

--- vocabularies/maotao/toskos.sh
+++ vocabularies/maotao/toskos.sh
@@ -2,12 +2,12 @@
 
 #Skosify installed with "sudo pip3 install --upgrade skosify"
 
-INFILES="maotao.ttl maotao-metadata.ttl ysoKehitys-2023-Hypatia.ttl"
+INFILES="maotao.ttl maotao-metadata.ttl ysoKehitys-2024-Jung.ttl"
 OUTFILE=maotao-skos.ttl
 
 CONFFILE="finnonto.cfg"
 LOGFILE=skosify.log
 
 #skosify -c $CONFFILE $INFILES -o $OUTFILE 2>$LOGFILE
-~/ontology/SKOSIFY/Skosify-master/skosify.py -c $CONFFILE $INFILES -o $OUTFILE 2>$LOGFILE
+python3 ~/ontology/SKOSIFY/Skosify-master/skosify.py -c $CONFFILE $INFILES -o $OUTFILE 2>$LOGFILE
 
