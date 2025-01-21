cf2ad3f7d9b5c8cdda220a10e5dd7fcd47e48abe
Finto-data <finto-posti@helsinki.fi>
2025-01-21 11:20:08 +0200

    Päivitetty OKM-tieteenalaluokituksen metatietoja

--- vocabularies/okm-tieteenala/toskos.sh
+++ vocabularies/okm-tieteenala/toskos.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
-pdftotext -f 3 -layout OKM*.pdf
+# Disabled this line in favour of storing the txt file in git / joelitak 2025-01-21
+# pdftotext -f 3 -layout OKM*.pdf
 ./okm-toskos.py OKM*.txt OKM*.csv >okm-tieteenala.ttl
 
 INFILES="okm-tieteenala.ttl okm-tieteenala-metadata.ttl"
 OUTFILE=okm-tieteenala-skos.ttl
 
-SKOSIFYHOME="../../tools/skosify/"
+SKOSIFYCMD="skosify"
 LOGFILE=skosify.log
 OPTS=""
 
-$SKOSIFYHOME/skosify.py $OPTS $INFILES -o $OUTFILE 2>$LOGFILE
+$SKOSIFYCMD $OPTS $INFILES -o $OUTFILE 2>$LOGFILE
