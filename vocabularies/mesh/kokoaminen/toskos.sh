2ae42d7740d8dfb35fc2e53018939dbb466af9bd
Joeli Takala <joeli.takala@helsinki.fi>
2025-08-01 18:27:17 +0300

    Päivitetty meshin kokoamisohjeet

--- vocabularies/mesh/kokoaminen/toskos.sh
+++ vocabularies/mesh/kokoaminen/toskos.sh
@@ -1,33 +1,19 @@
 #!/bin/sh
 
-INFILES="mesh-metadata.ttl mesh-skos.nt finmesh.ttl swemesh.ttl altlabels.ttl"
+INFILES="mesh-metadata.ttl mesh-skos.nt finmesh.ttl altlabels.ttl"
+
+# mesh-skos.nt : tuotettu hdt:lla, ks ohjeet.txt
+# finmesh.ttl : tuotettu Terkon sanalistasta, joka on muodossa prefLabel@en, prefLabel@fi, mahdolliset altLabel@fi
+# altlabels.ttl : tuotettu KI:n synonyymilistasta CSV:stä RDF:ksi
+
 OUTFILE=mesh-skos.ttl
-SKOSIFYHOME="../../../Skosify"
+SKOSIFYCMD="skosify"
 CONFFILE="../../conf/skosify/finnonto.cfg"
 LOGFILE=skosify.log
 
-# running sparql constructs against the .hdt
-echo 'Constructing...'
-echo 'prefLabels'
-~/bin/hdt-java-package-2.0-SNAPSHOT/bin/hdtsparql.sh mesh2017.hdt "`cat 1-prefLabels.rq`" > mesh-skos.nt
-echo 'altLabels'
-~/bin/hdt-java-package-2.0-SNAPSHOT/bin/hdtsparql.sh --stream mesh2017.hdt "`cat 2-altLabels.rq`" >> mesh-skos.nt
-echo 'scopeNotes'
-~/bin/hdt-java-package-2.0-SNAPSHOT/bin/hdtsparql.sh mesh2017.hdt "`cat 3-scopeNotes.rq`" >> mesh-skos.nt
-echo 'dates'
-~/bin/hdt-java-package-2.0-SNAPSHOT/bin/hdtsparql.sh mesh2017.hdt "`cat 4-dates.rq`" >> mesh-skos.nt
-echo 'broaders'
-~/bin/hdt-java-package-2.0-SNAPSHOT/bin/hdtsparql.sh mesh2017.hdt "`cat 5-broaders.rq`" >> mesh-skos.nt
-echo 'relateds'
-~/bin/hdt-java-package-2.0-SNAPSHOT/bin/hdtsparql.sh mesh2017.hdt "`cat 6-relateds.rq`" >> mesh-skos.nt
 
-echo 'Converting URIs...'
-# converting the uris
-sed -i .bak -e 's|http://id.nlm.nih.gov/mesh/2017/|http://www.yso.fi/onto/mesh/|g' mesh-skos.nt
-
-~/bin/hdt-java-package-2.0-SNAPSHOT/bin/hdtsparql.sh mesh2017.hdt "`cat exactmatches.rq`" >> mesh-skos.nt
 
 echo 'Running skosify...'
-$SKOSIFYHOME/skosify/skosify.py -c $CONFFILE $INFILES -o $OUTFILE 2>$LOGFILE
+$SKOSIFYCMD -c $CONFFILE $INFILES -o $OUTFILE 2>$LOGFILE
 
 echo 'Done!'
