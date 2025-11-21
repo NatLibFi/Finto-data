f73c2bd82992245af1fc52996fbf8dec36bb6ed9
Joeli Takala <joeli.takala@helsinki.fi>
2025-11-21 15:35:11 +0200

    Revert "JUHO päivitetty (Laotse)"
    This reverts commit 391905346e01799d60a473b2336d74ee78098258.

--- vocabularies/juho/toskos.sh
+++ vocabularies/juho/toskos.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-INFILES="juho-metadata.ttl juho-singular.ttl juho.ttl ../yso/releases/2025.7.Laotse/ysoKehitys.rdf"
+INFILE="~/codes/RDFTools/juho-uusi-yritys-2022-10-21/juho-skripti-ajettu-1-puritettu-plus-manuaaliset.ttl"
+INFILES="juho-metadata.ttl juho-singular.ttl ../yso/releases/2020.1.Diotima/ysoKehitys.rdf $INFILE"
 OUTFILE=juho-skos.ttl
 
 SKOSIFYCMD=skosify
