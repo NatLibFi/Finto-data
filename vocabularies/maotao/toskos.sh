4b39a43eac29e5b292d8f1503f9d8bf2f655ab3e
miguelvaara <mika.vaara@helsinki.fi>
2025-12-15 16:45:08 +0200

    Revert "Lisää TSK:n valmiiksi muodostama MAOTAO"
    This reverts commit bb7b65ccdbaa9b93435eba13ee42643e496169ab.

--- vocabularies/maotao/toskos.sh
+++ vocabularies/maotao/toskos.sh
@@ -2,12 +2,12 @@
 
 #Skosify installed with "sudo pip3 install --upgrade skosify"
 
-INFILES="maotao.ttl maotao-metadata.ttl  ysoKehitys-2025-7-Laotse.ttl"
+INFILES="maotao.ttl maotao-metadata.ttl ysoKehitys-2024-Jung.ttl"
 OUTFILE=maotao-skos.ttl
 
 CONFFILE="finnonto.cfg"
 LOGFILE=skosify.log
 
 #skosify -c $CONFFILE $INFILES -o $OUTFILE 2>$LOGFILE
-~/ontology/python3venv/bin/python3 ~/ontology/SKOSIFY/Skosify-master/skosify.py -c $CONFFILE $INFILES -o $OUTFILE 2>$LOGFILE
+python3 ~/ontology/SKOSIFY/Skosify-master/skosify.py -c $CONFFILE $INFILES -o $OUTFILE 2>$LOGFILE
 
