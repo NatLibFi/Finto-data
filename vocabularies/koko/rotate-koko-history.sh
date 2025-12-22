210d5914a7ddeef63a0a8d27282349e9435ccd93
Joeli Takala <joeli.takala@helsinki.fi>
2025-12-22 16:59:39 +0200

    KOKO päivitetty

---
+++ vocabularies/koko/rotate-koko-history.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Roll the history forwards
+cp koko-test.ttl koko.ttl
+cp koko-skos_edellinen.ttl koko-skos_edellinen-edellinen.ttl
+cp koko-skos.ttl koko-skos_edellinen.ttl
+cp koko-skos_test.ttl koko-skos.ttl
