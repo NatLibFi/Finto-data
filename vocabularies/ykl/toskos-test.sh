97160322b90a20da551bc45ffe92e7a09d728963
Finto-data <finto-posti@helsinki.fi>
2026-02-19 16:40:05 +0200

    YKL-testiversion julkaisu

--- vocabularies/ykl/toskos-test.sh
+++ vocabularies/ykl/toskos-test.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-RAWFILE="ykl-test.ttl"
+RAWFILE="ykl-test.rdf"
 
 SKOSIFYCMD="skosify"
 
@@ -8,6 +8,6 @@ INFILES="ykl-metadata.ttl ykl-hklj.ttl"
 OUTFILE=ykl-test-skos.ttl
 
 LOGFILE=skosify-test.log
-OPTS="-c ykl.cfg --namespace http://urn.fi/URN:NBN:fi:au:ykl: --mark-top-concepts"
+OPTS="-c ykl.cfg --namespace http://urn.fi/URN:NBN:fi:au:ykl: --label YKL --mark-top-concepts"
 
 $SKOSIFYCMD $OPTS $INFILES -o $OUTFILE 2>$LOGFILE
