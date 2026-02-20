549d0dc205895f69dbb869485c99e14c046b9ce0
Joeli Pokkinen <joeli.pokkinen@helsinki.fi>
2026-02-20 14:27:52 +0200

    Käytetään erillistä skosify-konfiguraatiota VB-YKL:lle

--- vocabularies/ykl/toskos-test.sh
+++ vocabularies/ykl/toskos-test.sh
@@ -6,6 +6,6 @@ INFILES="ykl-test.rdf ykl-metadata.ttl ykl-hklj.ttl"
 OUTFILE=ykl-test-skos.ttl
 
 LOGFILE=skosify-test.log
-OPTS="-c ykl.cfg --namespace http://urn.fi/URN:NBN:fi:au:ykl: --label YKL --mark-top-concepts"
+OPTS="-c ykl-test.cfg --namespace http://urn.fi/URN:NBN:fi:au:ykl: --label YKL --mark-top-concepts"
 
 $SKOSIFYCMD $OPTS $INFILES -o $OUTFILE 2>$LOGFILE
