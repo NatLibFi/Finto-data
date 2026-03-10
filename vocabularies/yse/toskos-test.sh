13b90ebf62593a7a1c4d45fcd539cf033fae76c7
Joeli Pokkinen <joeli.pokkinen@helsinki.fi>
2026-03-10 11:41:16 +0200

    YSEn testiversion skosify-komento

---
+++ vocabularies/yse/toskos-test.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+SKOSIFYCMD="skosify"
+INFILES="yse-test-metadata.ttl yse-test.ttl"
+OUTFILE=yse-test-skos.ttl
+
+SKOSIFYHOME="../../tools/skosify/"
+LOGFILE=skosify.log
+OPTS="--no-mark-top-concepts --no-enrich-mappings --no-aggregates"
+
+$SKOSIFYCMD $OPTS $INFILES >$OUTFILE 2>$LOGFILE
