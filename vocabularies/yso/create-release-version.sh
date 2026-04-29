cb6041e8a40597f3f4da6a0c38773c2559629478
Joeli Pokkinen <joeli.pokkinen@helsinki.fi>
2026-04-29 15:31:13 +0300

    Päivitetty YSOn jäädytetyn version julkaisukomentosarjaa

--- vocabularies/yso/create-release-version.sh
+++ vocabularies/yso/create-release-version.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
 # Aja komento sh create-release-version.sh Nimi
-# Anna nimi esim. muodossa Aristoteles
+# (Anna nimi esim. muodossa Aristoteles)
+
 # Tämä komentosarja tekee yson jäädytetyn version työvaiheet.
 # Tarkista lopputulos ennen versionhallintaan tallennusta!
-# joelitak 2026.02.06
+# joelitak 2026.04.29
 
 # Tarkistetaan, että käyttäjä on antanut nimen parametrina
+
+set -e
+
 if [ $# -ne 1 ]; then
     echo "Käyttö: $0 Nimi"
     exit 1
@@ -26,47 +30,24 @@ NIMI_LOWER=$(echo "$NIMI_PARAM" | tr '[:upper:]' '[:lower:]')
 
 DIR=releases/${V}.${K}.${Nimi}
 mkdir $DIR
-cp ysoKehitys.rdf $DIR/ysoKehitys.temp.rdf
+cp ysoKehitys.rdf $DIR/.
 cp yso-skos.ttl $DIR/.
 cp ../yso-paikat/yso-paikat-vb-dump.rdf $DIR/yso-paikat.rdf
 cp ../yso-paikat/yso-paikat-skos.ttl $DIR/.
 cd $DIR
 
-# Poistetaan TBC-kehitysversiosta apuluokat
-cat <<EOF > remove-dev-properties.rq
-PREFIX yso-meta: <http://www.yso.fi/onto/yso-meta/2007-03-02/>
-PREFIX yso-translate: <http://www.yso.fi/onto/yso-translate/>
-PREFIX yso-update: <http://www.yso.fi/onto/yso-update/>
-
-CONSTRUCT { ?s ?p ?o . }
-WHERE {
-  ?s ?p ?o .
-  FILTER(!STRSTARTS(STR(?s),STR(yso-update:))) .
-  FILTER(!STRSTARTS(STR(?o),STR(yso-update:))) .
-  FILTER(!STRSTARTS(STR(?s),STR(yso-translate:))) .
-  FILTER(!STRSTARTS(STR(?o),STR(yso-translate:))) .
-  FILTER(!STRSTARTS(STR(?p),STR(yso-meta:developmentComment))) .
-}
-EOF
-
 cat <<EOF > README.md
-Tämä on Yleisen Suomalaisen Ontologian (YSO) julkaisu ${V}.${K}.${Nimi}. Nämä tiedostot kuvastavat YSOn tilaa ${P}. ${K}. ${V}. YSO-${Nimi} on erityisesti erikoisontologioiden kehittäjille suunnattu versio YSOsta. Tiedostot ysoKehitys.rdf ja ysoKehitys.ttl ovat optimoitu TopBraid Composer-editointityökalulla tapahtuvaa ontologiatyötä ajatellen, ja se poikkeaa tietomalliltaan joiltain osin YSOn Fintosta löytyvästä julkaisuversiosta. YSOn ${Nimi}-versio on sisällöltään "jäädytetty", eli siihen ei päivitetä YSOn julkaisuversiossa näkyviä jatkuvia muutoksia. Versio sisältää YSOn keskeiset viime vuosien aikana toteutetut sisällölliset uudistukset.
+Tämä on Yleisen Suomalaisen Ontologian (YSO) julkaisu ${V}.${K}.${Nimi}. Nämä tiedostot kuvastavat YSOn tilaa ${P}. ${K}. ${V}. YSO-${Nimi} on erityisesti erikoisontologioiden kehittäjille suunnattu versio YSOsta. Tiedostot ysoKehitys.ttl ja ysoKehitys.rdf on optimoitu VocBench-editointityökalulla tapahtuvaa ontologiatyötä ajatellen. Nämä poikkeavat tietomalliltaan joiltain osin YSOn Fintosta löytyvästä julkaisuversiosta. YSOn kielimallien opettamiseen käytetty versio yso-combined sisältää yhdistelmän YSOn ja YSO-paikkojen julkaisuversioista. YSOn ${Nimi}-versio on sisällöltään "jäädytetty", eli siihen ei päivitetä YSOn julkaisuversiossa näkyviä jatkuvia muutoksia. Versio sisältää YSOn viimeisimmät sisällölliset uudistukset.
 EOF
-
-echo "Suoritetaan sparql-muokkaukset..."
-
-sparql --data ysoKehitys.temp.rdf --query remove-dev-properties.rq > temp.ttl
-rapper -i turtle -o rdfxml-abbrev temp.ttl > ysoKehitys.rdf 2>/dev/null
 
 echo "Muodostetaan julkaisutiedostot..."
 
 riot -formatted turtle yso-paikat.rdf > yso-paikat.ttl
+riot -formatted turtle ysoKehitys.rdf > ysoKehitys.ttl
 riot -out rdfxml-abbrev yso-skos.ttl yso-paikat-skos.ttl  > yso-combined.rdf
 riot -formatted turtle yso-skos.ttl yso-paikat-skos.ttl  > yso-combined.ttl
 
-rm ysoKehitys.temp.rdf temp.ttl remove-dev-properties.rq
-
-echo -e "\nYSOn jäädytetty versio on luotu. Voit nyt lisätä muutokset versionhallintaan. Lisää vielä Skosmoksen config.ttl-tiedostoon seuraava tietue:\n"
+echo -e "\nYSOn jäädytetty versio on luotu. Tarkista vielä tiedostot ennenkuin lisäät muutokset versionhallintaan. Voit lisätä vielä Skosmoksen config.ttl-tiedostoon seuraavan tietueen:\n"
 cat <<EOF
 :yso${Nimi} a skosmos:Vocabulary, void:Dataset ;
     dc:title "YSO - Yleinen suomalainen ontologia, ${Nimi}-versio"@fi,
