f7947674d4feadba0e4a5be38012f13561770e66
Joeli Pokkinen <joeli.pokkinen@helsinki.fi>
2026-04-29 16:25:01 +0300

    YKL-test nimi vaihdettu YKL-vb:ski

--- vocabularies/ykl/toskos-vb.sh
+++ vocabularies/ykl/toskos-vb.sh
@@ -4,9 +4,9 @@
 
 SKOSIFYCMD="skosify"
 
-INFILES="ykl-metadata.ttl ykl-test.rdf ykl-hklj.ttl"
-OUTFILE="ykl-test-skos.ttl"
-LOGFILE="skosify-test.log"
-OPTS="-c ykl-test.cfg --namespace http://urn.fi/URN:NBN:fi:au:ykl: --mark-top-concepts -F turtle"
+INFILES="ykl-metadata.ttl ykl-vb.rdf ykl-hklj.ttl"
+OUTFILE="ykl-vb-skos.ttl"
+LOGFILE="skosify-vb.log"
+OPTS="-c ykl-vb.cfg --namespace http://urn.fi/URN:NBN:fi:au:ykl: --mark-top-concepts -F turtle"
 
 $SKOSIFYCMD $OPTS $INFILES >$OUTFILE 2>$LOGFILE
