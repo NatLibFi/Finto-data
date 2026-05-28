72d555d962de3baa125941c5226026a0eb86dab3
miguelvaara <mika.vaara@helsinki.fi>
2026-05-28 13:23:26 +0300

    Lisää skriptiin hasPaivitetty-propertyyn liittyvät osat ja selkeytä skriptiä

--- vocabularies/juho/convert-to-vb/juho-to-vb-model.sh
+++ vocabularies/juho/convert-to-vb/juho-to-vb-model.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -euo pipefail
-UPDATE='/home/mijuahon/Softs/apache-jena-5.6.0/bin/update'
+UPDATE='apache-jena-5.6.0/bin/update'
 
 if [ "$#" -ne 3 ]; then
     echo "Käyttö: $0 <input.ttl> <update.rq> <output.ttl>"
@@ -9,15 +9,26 @@ if [ "$#" -ne 3 ]; then
     exit 1
 fi
 
+# ../juho.ttl
+ORIG_VOC="$1"
+# ./remove_asiasanalaji.rq
+THE_FIRST_SCRIPT="$2"
+# ./juho-asiasanalaji-removed.ttl
+ASIASANALAJI_REMOVED="$3"
 echo "Poistetaan juho-meta:hasAsiasanalaji"
-$UPDATE --data "$1" --update "$2" --dump > "$3"
+$UPDATE --data "$ORIG_VOC" --update "$THE_FIRST_SCRIPT" --dump > "$ASIASANALAJI_REMOVED"
+echo "juho-meta:hasAsiasanalaji poistettu ja päivitetty data tiedostossa $ASIASANALAJI_REMOVED."
 
-echo "juho-meta:hasAsiasanalaji poistettu ja päivitetty data tiedostossa $3."
-
-IMPORT_FOR_HASLAHDE=$3
-HASLAHDE=./remove_hasLahde.rq
+IMPORT_FOR_HASLAHDE=$ASIASANALAJI_REMOVED
+HASLAHDE_RQ="./remove_hasLahde.rq"
 HASLAHDE_REMOVED="./juho-haslahde-removed.ttl"
 echo "Poistetaan juho-meta:hasLahde"
-$UPDATE --data "$IMPORT_FOR_HASLAHDE" --update "$HASLAHDE" --dump > "$HASLAHDE_REMOVED"
-
+$UPDATE --data "$IMPORT_FOR_HASLAHDE" --update "$HASLAHDE_RQ" --dump > "$HASLAHDE_REMOVED"
 echo "juho-meta:hasLahde poistettu ja päivitetty data tiedostossa $HASLAHDE_REMOVED."
+
+IMPORT_FOR_HAS_PAIVITETTY=$HASLAHDE_REMOVED
+HASPAIVITETTY_RQ='./copy_hasPaivitetty_to_skos_editorial_note_and_remove_hasPaivitetty.rq'
+HASPAIVITETTY_REMOVED_AND_COPIED_TO_NOTE='./juho-hasPaivitetty-removed-and-note-added.ttl'
+echo "Poistetaan juho-meta:hasPaivitetty ja kopioidaan arvot editorialNoteihin"
+$UPDATE --data "$IMPORT_FOR_HAS_PAIVITETTY" --update "$HASPAIVITETTY_RQ" --dump > "$HASPAIVITETTY_REMOVED_AND_COPIED_TO_NOTE"
+echo "Juho-meta:hasPaivitetty poistettu ja arvot kopioitu editorialNoteihin $HASPAIVITETTY_REMOVED_AND_COPIED_TO_NOTE."
