c768096242edaf29b3a2273437dcbd6d5c132c31
miguelvaara <mika.vaara@helsinki.fi>
2026-05-28 11:21:19 +0300

    Lisää juho-meta:hasLahde-ominaisuuden ja sen käyttötapaukset poistavat osat muuntoskriptiin

--- vocabularies/juho/convert-to-vb/juho-to-vb-model.sh
+++ vocabularies/juho/convert-to-vb/juho-to-vb-model.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 set -euo pipefail
-UPDATE='/apache-jena-5.6.0/bin/update'
+UPDATE='/home/mijuahon/Softs/apache-jena-5.6.0/bin/update'
 
 if [ "$#" -ne 3 ]; then
     echo "Käyttö: $0 <input.ttl> <update.rq> <output.ttl>"
+    echo "Esim: ./juho-to-vb-model.sh ../juho.ttl ./remove_asiasanalaji.rq juho-asiasanalaji-removed.ttl"
+    echo "Tämä ./remove_asiasanalaji.rq on aina pakollinen"
     exit 1
 fi
 
@@ -11,3 +13,11 @@ echo "Poistetaan juho-meta:hasAsiasanalaji"
 $UPDATE --data "$1" --update "$2" --dump > "$3"
 
 echo "juho-meta:hasAsiasanalaji poistettu ja päivitetty data tiedostossa $3."
+
+IMPORT_FOR_HASLAHDE=$3
+HASLAHDE=./remove_hasLahde.rq
+HASLAHDE_REMOVED="./juho-haslahde-removed.ttl"
+echo "Poistetaan juho-meta:hasLahde"
+$UPDATE --data "$IMPORT_FOR_HASLAHDE" --update "$HASLAHDE" --dump > "$HASLAHDE_REMOVED"
+
+echo "juho-meta:hasLahde poistettu ja päivitetty data tiedostossa $HASLAHDE_REMOVED."
