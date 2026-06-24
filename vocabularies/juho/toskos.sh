dd9e81666a8ddf63dfae34b7577e68c464216cb8
miguelvaara <mika.vaara@helsinki.fi>
2026-06-24 11:13:17 +0300

    Viimeistele toskos.sh ohjeistuksen osalta, tee lopulliset muutokset skosify-ajossa käytettyyn konfiguraatioon sekä lataa uusimmassa ajossa muodostettu testitiedosto devillä käytettäväksi + ajon loki

--- vocabularies/juho/toskos.sh
+++ vocabularies/juho/toskos.sh
@@ -1,11 +1,23 @@
 #!/bin/sh
 
-INFILES="juho-metadata.ttl juho-singular.ttl juho.ttl ../yso/releases/2025.7.Laotse/ysoKehitys.rdf"
-OUTFILE=juho-skos.ttl
+set -e
+
+# Vaihda tähän oma Python3-virtuaaliympäristösi, jossa on asennettuna skosify.
+# Mikäli se puuttuu, asenna se pip:illä käyttäen komentoa:
+# pip install skosify
+. ~/codes/venvs/forthree/bin/activate
+
+# Tämä edellyttää, että olet ensin ajanut skriptin juho-vb-deprecator.sh (löytyy toolseista), joka tuottaa tiedoston juho-vb-deprecator-out.ttl.
+# arq on Apache Jena -työkalu, muuta sen käynnistys työskentely-ympäristössäsi käyttämäsi tavan mukaiseksi.
+arq --data juho-vb-deprecator-out.ttl --query extract-juho.rq > juho-irrotettu.ttl
+
+INFILES="juho-metadata.ttl juho-singular.ttl juho-irrotettu.ttl ../yso/releases/2026.3.Maimonides/yso-skos.ttl"
+OUTFILE="./juho-skos-vb.ttl"
+TMPFILE="./juho-skos-vb-tmp.ttl"
 
 SKOSIFYCMD=skosify
-CONFFILE=../../conf/skosify/finnonto.cfg
 LOGFILE=skosify.log
 OPTS="-l fi"
 
-$SKOSIFYCMD $OPTS -c $CONFFILE -o $OUTFILE $INFILES 2>$LOGFILE
+echo "Ajetaan skosify konfiksella juho-vb.cfg..."
+$SKOSIFYCMD $OPTS -c ../../conf/skosify/juho-vb.cfg -o $OUTFILE $INFILES 2>$LOGFILE
