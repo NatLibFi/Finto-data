f8fa5b31497d9a7ce4032e33f941ebc9dbd3d618
miguelvaara <mika.vaara@helsinki.fi>
2026-08-14 15:10:36 +0300

    Päivitä LIIKO

--- vocabularies/liiko/toskos.sh
+++ vocabularies/liiko/toskos.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-INFILES="mero.ttl mero-metadata.ttl ysoKehitys-2022-Filolaos.ttl"
+INFILES="mero.ttl mero-metadata.ttl ysoKehitysTBC_2026-3_Maimonides.ttl"
 OUTFILE=mero-skos.ttl
 
-SKOSIFYHOME="/home/terminologi/ontology/SKOSIFY"
 CONFFILE=finnonto.cfg
 LOGFILE=skosify.log
 
 $SKOSIFYHOME/Skosify-master/skosify.py -c $CONFFILE $INFILES -o $OUTFILE 2>$LOGFILE
+~/ontology/python3venv/bin/python3 ~/ontology/SKOSIFY/Skosify-master/skosify.py -c $CONFFILE $INFILES -o $OUTFILE 2>$LOGFILE
 
