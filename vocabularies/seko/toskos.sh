4073529bd0af34109d0b73eb019cb8ba993a1ac9
Vainonen <8686308+Vainonen@users.noreply.github.com>
2026-09-25 11:19:37 +0300

    Tuore versio Sekosta Alephista haettuna ja uudet skriptit tätä varten

--- vocabularies/seko/toskos.sh
+++ vocabularies/seko/toskos.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-INFILES="seko-metadata.ttl seko-decoded.ttl"
+python fetch-asteri.py "" >seko.mrcx 2>fetch-asteri.log
+python oai-pmh-to-skos.py http://urn.fi/urn:nbn:fi:au:seko: seko <seko.mrcx >seko.ttl 2>convert.log
+
+INFILES="seko-metadata.ttl seko.ttl seko-decoded.ttl"
 OUTFILE=seko-skos.ttl
 
 SKOSIFYCMD="skosify"
@@ -10,3 +13,4 @@ LOGFILE=skosify.log
 /usr/local/virtualenvs/skosify/bin/python3 unidecode-hidden-labels.py seko.ttl>seko-decoded.ttl
 $SKOSIFYCMD -c $CONFFILE $INFILES -o $OUTFILE 2>$LOGFILE
 rm seko-decoded.ttl
+rm seko.mrcx
